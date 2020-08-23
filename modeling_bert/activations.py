5168e7946e2ca00481386e59e2d38fc4b212328e
Vishal Burman <vishal.a.burman23@gmail.com>
2020-08-23 12:36:17 +0530

    Added [SWISH], [GELU], [GELU_NEW] activations

---
+++ modeling_bert/activations.py
@@ -0,0 +1,35 @@
+import logging
+import math
+
+import torch
+import torch.nn.functional as F
+
+logger = logger.getLogger(__name__)
+
+gelu = F.gelu
+
+def gelu_new(x):
+    """
+    Implementation of the gelu activation function currently in Google Bert repo
+
+    """
+
+    return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0/Math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
+
+
+def swish(x):
+    return x * torch.sigmoid(x)
+
+
+
+ACT2FN = {
+        "gelu": gelu,
+        "gelu_new": gelu_new,
+        "swish": swish
+        }
+
+def get_activation(activation_string):
+    if activation_string in ACT2FN:
+        return ACT2FN[activation_string]
+    else:
+        raise KeyError("Function not found!!!")
---
+++ modeling_bert/model.py
@@ -0,0 +1,11 @@
+import logging
+import math
+import os
+import warnings
+
+import torch
+import torch.utils.checkpoint
+from torch import nn
+from torch.nn import CrossEntropyLoss, MSELoss
+
+from
