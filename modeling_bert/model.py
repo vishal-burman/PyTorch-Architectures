23f57c787f317409867d3d211cf51468d57e34c7
Vishal Burman <vishal.a.burman23@gmail.com>
2020-08-28 14:01:55 +0530

    Added BertIntermediate module

--- modeling_bert/model.py
+++ modeling_bert/model.py
@@ -186,4 +186,19 @@ class BertAttention(nn.Module):
         outputs = (attention_output) + self.outputs[1:]
         return outputs
 
+class BertIntermediate(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.intermediate_size)
+        if isinstance(config.hidden_act, str):
+            self.intermediate_act_fn = ACT2FN[config.hidden_act]
+        else:
+            self.intermediate_act_fn = config.hidden_act
 
+    def forward(self, hidden_states):
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.intermediate_act_fn(hidden_states)
+        return hidden_states
+
+class BertOutput(nn.Module):
+    pass
