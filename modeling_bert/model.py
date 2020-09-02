8bc0700cddb9ea5e7b53bea6462b1c5f5dff0561
Vishal Burman <vishal.a.burman23@gmail.com>
2020-09-02 16:47:59 +0530

    Added BertModel

--- modeling_bert/model.py
+++ modeling_bert/model.py
@@ -409,7 +409,70 @@ class BertModel(BertPretrainedModel):
             output_hidden_states=None,
             return_dict=None,
             ):
-        pass
+
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+                output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+                )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if input_ids is not None and input_embeds is not None:
+            raise ValueError("Both input_ids and input_embeds cannot exist at the same time")
+        elif input_ids is not None:
+            input_shape = input_ids.size()
+        elif input_embeds is not None:
+            input_shape = input_embeds.size()[:-1]
+        else:
+            raise ValueError("You have to specify either input_ids or input_embeds")
+
+        device = input_ids.device if input_ids is not None else input_embeds.device
+
+        if attention_mask is None:
+            attention_mask = torch.ones(input_shape, device=device)
+        if token_type_ids is None:
+            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=device)
+
+        # TODO
+        extended_attention_mask = self.get_extended_attention_mask(attention_mask, input_shape, device)
+
+        if self.config.is_decoder and encoder_hidden_states is not None:
+            encoder_batch_size, encoder_sequence_length = encoder_hidden_states.size()
+            encoder_hidden_shape = (encoder_batch_size, encoder_sequence_length)
+            if encoder_attention_mask is None:
+                encoder_attention_mask = torch.ones(encoder_hidden_shape, device=device)
+            # TODO
+            encoder_extended_attention_mask = self.invert_attention_mask(encoder_attention_mask)
+        else:
+            encoder_attention_mask = None
+
+        head_mask = self.get_head_mask(head_mask, self.config.num_hidden_layers)
+
+        embedding_output = self.embeddings(
+                input_ids=input_ids, position_ids=position_ids, token_type_ids=token_type_ids, input_embeds=input_embeds
+                )
+
+        encoder_outputs = self.encoder(
+                embedding_output,
+                attention_mask=extended_attention_mask,
+                head_mask=head_mask,
+                encoder_hidden_states=encoder_hidden_states,
+                encoder_attention_mask=encoder_extended_attention_mask,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+                )
+        sequence_output = encoder_outputs[0]
+        pooled_output = self.pooler(sequence_output)
+
+        if not return_dict:
+            return (sequence_output, pooled_output) + encoder_outputs[1:]
+
+        return BaseModelOutputWithPooling(
+                last_hidden_state=sequence_output,
+                pooler_output=pooled_output,
+                hidden_states=encoder_outputs.hidden_states,
+                attentions=encoder_outputs.attentions,
+                )
 
 
 
