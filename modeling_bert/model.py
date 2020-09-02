89e1dd888b833d5191c3b973af207de839081b29
Vishal Burman <vishal.a.burman23@gmail.com>
2020-09-02 11:19:03 +0530

    Added BertPredictionHeadTransform

--- modeling_bert/model.py
+++ modeling_bert/model.py
@@ -326,3 +326,19 @@ class BertPooler(nn.Module):
         pooled_output = self.dense(first_token_tensor)
         pooled_output = self.activation(pooled_output)
         return pooled_output
+
+class BertPredictionHeadTransform(nn.Module):
+    def  __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        if isinstance(config.hidden_act, str):
+            self.transform_act_fn = ACT2FN[config.hidden_act]
+        else:
+            self.transform_act_fn = config.hidden_act
+        self.LayerNorm = BertLayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+
+    def forward(self, hidden_states):
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.transform_act_fn(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states)
+        return hidden_states
