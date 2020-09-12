78606dca385aec5457e6cdd8301a6c5ddde31524
Vishal Burman <vishal.a.burman23@gmail.com>
2020-09-12 20:24:06 +0530

    Tested Tokenizer and CustomDataset for GPTModel

---
+++ modeling_openai/test_sample.py
@@ -0,0 +1,44 @@
+############################
+# Ignore this file
+# Used for testing purposes
+############################
+
+import torch
+import torch.nn
+from torch.utils.data import DataLoader, Dataset
+from transformers import OpenAIGPTTokenizer
+
+tokenizer = OpenAIGPTTokenizer.from_pretrained("openai-gpt")
+# pad_token is not set by default
+tokenizer.pad_token = '[PAD]'
+
+class CustomDataset(Dataset):
+
+    def __init__(self, texts, tokenizer):
+        self.texts = texts
+        self.tokenizer = tokenizer
+        self.list_texts = []
+        self.build()
+
+    def __len__(self):
+        return len(self.list_texts)
+
+    def __getitem__(self, index):
+        ids = self.list_texts[index]['input_ids']
+        mask = self.list_texts[index]['attention_mask']
+        
+        return{
+                'ids': torch.tensor(ids, dtype=torch.long),
+                'mask': torch.tensor(mask, dtype=torch.long)
+                }
+
+    def build(self):
+        for text in self.texts:
+            self.list_texts.append(self.tokenizer(text, max_length=32, pad_to_max_length=True, truncation=True))
+
+texts = ["this is my home", "that movie looks good", "this is a great book!", "what is your name?"]
+dataset = CustomDataset(texts, tokenizer)
+data_loader = DataLoader(dataset, shuffle=False, batch_size=2)
+for sample in data_loader:
+    print(sample['ids'].shape)
+    break
