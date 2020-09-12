e2b6a34b6213526f3c08ca1306b940d166c3a2be
Vishal Burman <vishal.a.burman23@gmail.com>
2020-09-12 12:46:36 +0530

    Complete [GPTLMHeadModel]

--- modeling_openai/model.py
+++ modeling_openai/model.py
@@ -42,8 +42,7 @@ class OpenAIGPTLMHeadModel(OpenAIGPTPretrainedModel):
         self.init_weights()
 
     def get_output_embeddings(self):
-        # TODO
-        pass
+        return self.lm_head
 
     def forward(
             self,
@@ -60,4 +59,31 @@ class OpenAIGPTLMHeadModel(OpenAIGPTPretrainedModel):
             ):
         # TODO
 
-        pass
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        
+        transformer_outputs = self.transformer(
+                input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                position_ids=position_ids,
+                head_mask=head_mask,
+                inputs_embeds=inputs_embeds,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+                )
+        hidden_states = transformer_outputs[0]
+        lm_logits = self.lm_head(hidden_states)
+
+        loss = None
+        if labels is not None:
+            # Shift so that tokens < n predict n
+            shift_logits = lm_logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            # Flatten the tokens 
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+
+        if not return_dict:
+            output = (lm_logits,) + transformer_outputs[1:]
+            return ((loss,) + output) if loss is not None else output
