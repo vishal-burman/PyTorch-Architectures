841562d42ac3133c7e623c09bcab10ee22517954
Vishal Burman <vishal.a.burman23@gmail.com>
2020-09-14 07:45:51 +0530

    Added [Attention] Module

--- modeling_openai/model.py
+++ modeling_openai/model.py
@@ -4,22 +4,72 @@ import torch.nn as nn
 class Attention(nn.Module):
     def __init__(self, nx, n_ctx, config, scale=False):
         super().__init__()
-        pass
+        n_state = nx
 
-    def prune_heads(self, heads):
-        pass
+        assert n_state % config.n_head == 0
+        self.register_buffer("bias", torch.tril(torch.ones(n_ctx, n_ctx)).view(1, 1, n_ctx, n_ctx))
+        self.n_head = config.n_head
+        self.split_size = n_state
+        self.scale = scale
+
+        self.c_attn = Conv1D(n_state * 3, nx)
+        self.c_proj = Conv1D(n_state, nx)
+        self.attn_dropout = nn.Dropout(config.attn_pdrop)
+        self.resid_dropout = nn.Dropout(config.resid_pdrop)
+        self.pruned_heads = set()
 
     def _attn(self, q, k, v, attention_mask=None, head_mask=None, output_attentions=False):
-        pass
+        w = torch.matmul(q, k)
+        if self.scale:
+            w = w / math.sqrt(v.size(-1))
+        b = self.bias[:, :, :w.size(-2), :w.size(-1)]
+        w = w * b + -1e4 * (1-b)
+
+        if attention_mask is not None:
+            # Apply the attention mask
+            w = w + attention_mask
+
+        w = nn.Softmax(dim=-1)(w)
+        w = self.attn_dropout(w)
+
+        # Mask heads if we want to
+        if head_mask is not None:
+            w = w * head_mask
+
+        outputs = [torch.matmul(w, v)]
+        if output_attentions:
+            outputs.append(w)
+        return outputs
 
     def merge_heads(self, x):
-        pass
+        x = x.permute(0, 2, 1, 3).contiguous()
+        new_x_shape = x.size()[:-2] + (x.size(-2) * x.size(-1),)
+        return x.view(*new_x_shape)
 
     def split_heads(self, x, k=False):
-        pass
+        new_x_shape = x.size()[:-1] + (self.n_head, x.size(-1) // self.n_head)
+        x = x.view(*new_x_shape)
+        if k:
+            return x.permute(0, 2, 3, 1)
+        else:
+            return x.permute(0, 2, 1, 3)
 
     def forward(self, x, attention_mask=None, head_mask=None, output_attentions=False):
-        pass
+        x = self.c_attn(x)
+        query, key, value = x.split(self.split_size, dim=2)
+        query = self.split_heads(query)
+        key = self.split_heads(key, k=True)
+        value = self.split_heads(value)
+
+        attn_outputs = self._attn(query, key, value, attention_mask, head_mask, output_attentions)
+        a = attn_outputs[0]
+
+        a = self.merge_heads(a)
+        a = self.c_proj(a)
+        a = self.resid_dropout(a)
+
+        outputs = [a] + attn_outputs[1:]
+        return outputs
 
 
 class MLP(nn.Module):
@@ -45,7 +95,12 @@ class Block(nn.Module):
         self.mlp = MLP(4*nx, config)
         self.ln_2 = nn.LayerNorm(nx, eps=config.layer_norm_epsilon)
 
-    def forward(self, x, attention_mask=None, head_mask=None, output_attentions=False):
+    def forward(self, 
+            x, # [batch_size, max_len]
+            attention_mask=None, 
+            head_mask=None, 
+            output_attentions=False):
+
         attn_outputs = self.attn(
                 x,
                 attention_mask=attention_mask,
