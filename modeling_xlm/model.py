45a97dded8182cd0a3515d5efa03ba6d312259f9
Vishal Burman <vishal.a.burman23@gmail.com>
2020-10-04 13:34:23 +0530

    model.py: embedding layer forward pass completed

--- modeling_xlm/model.py
+++ modeling_xlm/model.py
@@ -75,6 +75,55 @@ class XLMModel(XLMPretrainedModel):
             return_dict=None,
             ):
 
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+                output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+                )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if input_ids is not None:
+            bs, slen = input_ids.size()
+        else:
+            bs, slen = inputs_embeds.size()[:-1]
+
+        device = input_ids.device if input_ids is not None else inputs_embeds.device
+
+        if lengths is None:
+            if input_ids is not None:
+                lengths = (input_ids != self.pad_index).sum(dim=1).long()
+            else:
+                lengths = torch.tensor([slen] * bs, device=device)
+
+        # check inputs
+        assert lengths.size(0) == bs
+        assert lengths.max().item() <= slen
+
+        # generate masks TODO
+        mask, attn_mask = get_masks(slen, lengths, self.causal, padding_mask=attention_mask)
+
+        # do not recompute cached elements TODO needed ?
+        if cache is not None and input_ids is not None:
+            _slen = slen - cache['slen']
+            input_ids = input_ids[:, -_slen:]
+            position_ids = position_ids[:, -_slen:]
+            if langs is not None:
+                langs = langs[:, -_slen:]
+            attn_mask = attn_mask[:, -_slen:]
+
+        # embeddings
+        if inputs_embeds is None:
+            inputs_embeds = self.embedding(input_ids)
+
+        tensor = inputs_embeds + self.position_embeddings(position_ids).expand_as(inputs_embeds)
+        if langs is not None and self.use_lang_emb and self.n_langs > 1:
+            tensor = tensor + self.lang_embeddings(langs)
+        if token_type_ids is not None:
+            tensor = tensor + self.embeddings(token_type_ids)
+        tensor = self.layer_norm(tensor)
+        tensor = F.dropout(tensor, p=self.dropout, training=self.training)
+        tensor *= mask.unsqueeze(-1).to(tensor.dtype)
+
+
 
 
 class XLMForSequenceClassification(XLMPretrainedModel):
