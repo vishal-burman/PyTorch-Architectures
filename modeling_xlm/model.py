b0933eec0ae31d35f488cd334ee652d0f564df67
Vishal Burman <vishal.a.burman23@gmail.com>
2020-10-11 11:50:08 +0530

    model.py: XLMPretrained Model Completed

--- modeling_xlm/model.py
+++ modeling_xlm/model.py
@@ -1,3 +1,26 @@
+class XLMPretrainedModel(PretrainedModel):
+    config_class = XLMConfig
+    # TODO check if needed?
+    base_model_prefix = "transformer"
+
+    def __init__(self, *inputs, **kwargs):
+        super().__init__(*inputs, **kwargs)
+
+    def _init_weights(self, module):
+        """Initialize the weights."""
+        if isinstance(module, nn.Embedding):
+            if self.config is not None and self.config.embed_init_std is not None:
+                nn.init.normal_(module.weight, mean=0, std=self.config.embed_init_std)
+        if isinstance(module, nn.Linear):
+            if self.config is not None and self.config.init_std is not None:
+                nn.init.normal_(module.weight, mean=0, std=self.config.init_std)
+                if hasattr(module, "bias") and module.bias is not None:
+                    nn.init.constant_(module.bias, 0.0)
+        if isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+
+
 class XLMModel(XLMPretrainedModel):
     def __init__(self, config):
         super().__init__(config)
