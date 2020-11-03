fe7b16242d496cbee1ed935c339cd4e1742b212f
Vishal Burman <vishal.a.burman23@gmail.com>
2020-11-03 21:43:21 +0530

    model.py: [Residual] & [PreNorm] modules added

--- modeling_ViT/model.py
+++ modeling_ViT/model.py
@@ -1,6 +1,21 @@
 import torch
 import torch.nn as nn
 
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(x, **kwargs)
+
+class PreNorm(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
 class FeedForward(nn.Module):
     def __init__(self, dim, hidden_dim, dropout=0.):
         super().__init__()
