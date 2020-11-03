6c224630cd85936c401a4c0c271d1e8d52d52287
Vishal Burman <vishal.a.burman23@gmail.com>
2020-11-03 18:30:35 +0530

    model.py: Dimension changes & bug fixes

--- modeling_ViT/model.py
+++ modeling_ViT/model.py
@@ -5,13 +5,17 @@ class Attention(nn.Module):
     def __init__(self, dim, heads=8, dropout=0.):
         super().__init__()
         self.heads = heads
+        self.scale = dim ** -0.5
 
-class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, mlp_dim, dropout):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        pass
-    pass
+        self.qkv = nn.Linear(dim, dim*3, bias=False)
+        self.to_out = nn.Sequential(
+                nn.Linear(dim, dim),
+                nn.Dropout(dropout),
+                )
+
+    def forward(self, x, mask=None): # x ~ [batch_size, (img_size // patch_size) + 1, dim] || mask ~ [batch_size, img_size // patch_size, img_size // patch_size]
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim=-1)
 
 class ViT(nn.Module):
     def __init__(self, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, channels=3, dropout=0., emb_dropout=0.)
@@ -36,7 +40,7 @@ class ViT(nn.Module):
             nn.Linear(mlp_dim, num_classes),
             )
 
-    self.proj = nn.Conv2d(in_channels=3, out_channels=dim, kernel_size=self.patch_size, stride=self.patch_size)
+    self.proj = nn.Conv2d(in_channels=3, out_channels=patch_dim, kernel_size=self.patch_size, stride=self.patch_size)
 
     self.layers = nn.ModuleList([])
     for _ in range(depth):
@@ -45,15 +49,15 @@ class ViT(nn.Module):
             Residual(Prenorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))),
             ])
 
-    def forward(self, img, mask=None):
-        x = self.proj(x).flatten(2).transpose(1, 2)
-        x = self.patch_to_embedding(x)
-        b, n, _ = x.shape
+    def forward(self, img, mask=None): # img ~ [batch_size, channels, height, width] || mask ~ [batch_size, img_size // patch_size, img_size // patch_size]
+        x = self.proj(x).flatten(2).transpose(1, 2) # x ~ [batch_size, img_size // patch_size, patch_dim]
+        x = self.patch_to_embedding(x) # x ~ [batch_size, img_size // patch_size, dim]
+        b, n, _ = x.shape # b ~ batch_size || n ~ img_size // patch_size
 
-        cls_tokens = self.cls_token.expand(b, -1, -1)
-        x = torch.cat((cls_tokens, x), dim=1)
-        x += self.pos_embedding[:, :(n+1)]
-        x = self.dropout(x)
+        cls_tokens = self.cls_token.expand(b, -1, -1) # cls_tokens ~ [batch_size, 1, dim]
+        x = torch.cat((cls_tokens, x), dim=1) # x ~ [batch_size, (img_size // patch_size) + 1, dim]
+        x += self.pos_embedding[:, :(n+1)] # x ~ [batch_size, (img_size // patch_size) + 1, dim]
+        x = self.dropout(x) # x ~ [batch_size, (img_size // patch_size) + 1, dim]
 
         for attn, ff in self.layers:
             x = attn(x, mask=mask)
