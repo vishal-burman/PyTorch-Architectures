17377c4935eb4ac5484afad2a19398aebd274d9a
Vishal Burman <vishal.a.burman23@gmail.com>
2020-11-17 13:11:46 +0530

    model.py: weights init module removed

--- modeling_xlm/model.py
+++ modeling_xlm/model.py
@@ -113,33 +113,8 @@ class TransformerFFN(nn.Module):
         x = F.dropout(x, p=self.dropout, training=self.training)
         return x 
 
-class XLMPretrainedModel(nn.Module):
-    config_class = XLMConfig
 
-    def __init__(self, config, *inputs, **kwargs):
-        super().__init__()
-        self.config = config
-
-    def init_weights(self):
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        """Initialize the weights."""
-        if isinstance(module, nn.Embedding):
-            if self.config is not None and self.config.embed_init_std is not None:
-                nn.init.normal_(module.weight, mean=0, std=self.config.embed_init_std)
-        
-        if isinstance(module, nn.Linear):
-            if self.config is not None and self.config.init_std is not None:
-                nn.init.normal_(module.weight, mean=0, std=self.config.init_std)
-                if hasattr(module, "bias") and module.bias is not None:
-                    nn.init.constant_(module.bias, 0.0)
-        
-        if isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-class XLMModel(XLMPretrainedModel):
+class XLMModel(nn.Module):
     def __init__(self, config):
         super().__init__(config)
         self.n_words = config.vocab_size
