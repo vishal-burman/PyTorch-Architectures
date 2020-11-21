1f398ccfa421e732e219d3bb56a19bed5ef743a1
Vishal Burman <vishal.a.burman23@gmail.com>
2020-11-21 13:14:05 +0530

    model.py: Optimized [BertForSequenceClassification]

--- modeling_bert/model.py
+++ modeling_bert/model.py
@@ -540,64 +540,22 @@ class BertModel(BertPreTrainedModel):
         # return ~ ([batch_size, max_seq_len, emb_size], [batch_size, emb_size])
         return (sequence_output, pooled_output) + encoder_outputs[1:] 
 
-
-class BertForSequenceClassification(BertPreTrainedModel):
+class BertForSequenceClassification(nn.Module):
     def __init__(self, config):
         super().__init__(config)
         self.num_labels = config.num_labels
-
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
 
-        self.init_weights()
-
-    def forward(
-            self,
-            input_ids = None,
-            attention_mask = None,
-            token_type_ids = None,
-            position_ids = None,
-            head_mask = None,
-            inputs_embeds = None,
-            labels = None,
-            output_attentions = None,
-            output_hidden_states = None,
-            return_dict = None,
-            ):
-
-
-        # outputs ~ ([batch_size, max_seq_len, hidden_size], [batch_size, hidden_size])
-        outputs = self.bert(
-                input_ids,
-                attention_mask=attention_mask,
-                token_type_ids=token_type_ids,
-                position_ids=position_ids,
-                head_mask=head_mask,
-                inputs_embeds=inputs_embeds,
-                output_attentions=output_attentions,
-                output_hidden_states=output_hidden_states,
-                return_dict=return_dict,
-                )
-
-        # pooled_output ~ [batch_size, hidden_size]
-        pooled_output = outputs[1]
-        # pooled_output ~ [batch_size, hidden_size]
-        pooled_output = self.dropout(pooled_output)
-        # logits ~ [batch_size, num_labels]
-        logits = self.classifier(pooled_output)
-        
+    def forward(self, input_ids = None, attention_mask = None, labels = None):
+        outputs = self.bert(input_ids, attention_mask=attention_mask) # outputs ~ ([batch_size, max_seq_len, hidden_size], [batch_size, hidden_size])
+        pooled_output = outputs[1] # pooled_output ~ [batch_size, hidden_size]
+        pooled_output = self.dropout(pooled_output) # pooled_output ~ [batch_size, hidden_size]
+        logits = self.classifier(pooled_output) # logits ~ [batch_size, num_labels]
         loss = None
         if labels is not None:
-            if self.num_labels == 1:
-                loss_fct = MSELoss()
-                loss = loss_fct(logits.view(-1), labels.view(-1))
-            else:
-                loss_fct = CrossEntropyLoss()
-                #loss_fct = BCEWithLogitsLoss
-                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
-                #loss = BCEWithLogitsLoss()(logits.view(-1, self.num_labels), labels.view(-1))
-
-        
-        output = (logits,) + outputs[2:]
-        return ((loss,) + output) if loss is not None else output 
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+        output = (logits,)
+        return ((loss,) + output) if loss is not None else output  # return ~ (loss, [batch_size, num_labels])
