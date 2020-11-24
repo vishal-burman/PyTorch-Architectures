744eb8fc1d902884e98f261d782e913ba11ee5df
Vishal Burman <vishal.a.burman23@gmail.com>
2020-11-24 12:59:24 +0530

    model.py: Refactor LeNet to script

---
+++ modeling_LeNet/model.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LeNet(nn.Module):
+    def __init__(self, num_classes, grayscale = False):
+        super(LeNet, self).__init__()
+        
+        self.grayscale = grayscale
+        self.num_classes = num_classes
+        
+        if self.grayscale:
+            in_channels = 1
+        else:
+            in_channels = 3
+        
+        self.features = nn.Sequential(
+                nn.Conv2d(in_channels = in_channels, out_channels = 6*in_channels, kernel_size = 5),
+                nn.Tanh(),
+                nn.MaxPool2d(kernel_size = 2),
+                nn.Conv2d(in_channels = 6*in_channels, out_channels = 16*in_channels, kernel_size = 5),
+                nn.Tanh(),
+                nn.MaxPool2d(kernel_size = 2)
+        )
+        
+        self.classifier = nn.Sequential(
+                nn.Linear(16*5*5*in_channels, 120*in_channels),
+                nn.Tanh(),
+                nn.Linear(120*in_channels, 84*in_channels),
+                nn.Tanh(),
+                nn.Linear(84*in_channels, num_classes)
+        )
+    
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        logits = self.classifier(x)
+        probas = F.softmax(logits, dim = 1)
+        return logits, probas
+
