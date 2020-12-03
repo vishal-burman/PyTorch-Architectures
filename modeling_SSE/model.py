cd2b20a71d4c216420a60ee205ba43cc5a485aa3
Vishal Burman <vishal.a.burman23@gmail.com>
2020-12-03 18:15:31 +0530

    model.py: Refactor and bug fixes

--- modeling_SSE/model.py
+++ modeling_SSE/model.py
@@ -12,19 +12,20 @@ class SelfAttention(nn.Module):
         self.embedding_length = embedding_length
         self.weights = weights
 
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_length)
+        self.word_embeddings = nn.Embedding(self.vocab_size, self.embedding_length)
         if self.weights is not None:
-            self.word_embeddings.weights = nn.Parameter(weights, requires_grad=False)
-        self.dropout = 0.8
-        self.bilstm = nn.LSTM(embedding_length, hidden_size, dropout=self.dropout, bidirectional=True, num_layers=2)
-        self.W_s1 = nn.Linear(2*hidden_size, 350)
-        self.W_s2 = nn.Linear(350, 30)
-        self.fc_layer = nn.Linear(30*2*hidden_size, 2000)
-        self.label = nn.Linear(2000, output_size)
-        self.register_buffer("h_0", torch.zeros(4, self.batch_size, self.hidden_size))
-        self.register_buffer("c_0", torch.zeros(4, self.batch_size, self.hidden_size))
+            self.word_embeddings.weights = nn.Parameter(self.weights, requires_grad=False)
+        self.dropout = nn.Dropout(p=0.5)
+        self.bilstm = nn.LSTM(self.embedding_length, self.hidden_size, bidirectional=True)
+        self.W_s1 = nn.Linear(2*self.hidden_size, 64, bias=False)
+        self.W_s2 = nn.Linear(64, 1, bias=False)
+        self.fc_layer = nn.Linear(30*2*self.hidden_size, 3000)
+        self.label = nn.Linear(3000, self.output_size)
+        self.register_buffer("h_0", torch.zeros(2, self.batch_size, self.hidden_size))
+        self.register_buffer("c_0", torch.zeros(2, self.batch_size, self.hidden_size))
 
     def attention_net(self, lstm_output):
+        lstm_output = self.dropout(lstm_output)
         attn_weight_matrix = self.W_s2(torch.tanh(self.W_s1(lstm_output)))
         attn_weight_matrix = attn_weight_matrix.permute(0, 2, 1)
         attn_weight_matrix = F.softmax(attn_weight_matrix, dim=2)
