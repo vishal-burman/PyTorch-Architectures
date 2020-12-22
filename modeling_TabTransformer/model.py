5e1dbfd6558153dc530a71b2d5c216353b050541
Vishal Burman <vishal.a.burman23@gmail.com>
2020-12-22 15:46:01 +0530

    model.py: [TabTransformer] forward pass done

--- modeling_TabTransformer/model.py
+++ modeling_TabTransformer/model.py
@@ -39,3 +39,36 @@ class TabTransformer(nn.Module):
         self.num_unique_categories = sum(categories)
         self.num_special_tokens = num_special_tokens
         total_tokens = self.num_unique_categories + num_special_tokens
+        categories_offset = F.pad(torch.tensor(list(categories)), (1, 0), value = num_special_tokens)
+        categories_offset = categories_offset.cumsum(dim=-1)[:-1]
+        self.register_buffer('categories_offset', categories_offset)
+        if exists(continuous_mean_std):
+            self.register_buffer('continuous_mean_std', continuous_mean_std)
+        self.norm = nn.LayerNorm(num_continuous)
+        self.num_continuous = num_continuous
+        self.transformer = Transformer(
+                num_tokens = num_tokens,
+                dim = dim,
+                depth = depth,
+                heads = heads,
+                dim_head = dim_head,
+                attn_dropout = attn_dropout,
+                ff_dropout = ff_dropout,
+                )
+        input_size = (dim * self.num_categories) + num_continuous
+        l = input_size // 8
+        hidden_dimensions = list(map(lambda x: l * x, mlp_hidden_mults))
+        all_dimensions = [input_size, *hidden_dimensions, dim_out]
+        self.mlp = MLP(all_dimensions, act = mlp_act)
+    
+    def forward(self, x_categ, x_cont):
+        assert x_categ.shape[-1] == self.num_categories
+        x_categ += self.categories_offset
+        x = self.transformer(x_categ)
+        flat_categ = x.flatten(1)
+        if exists(self.continuous_mean_std):
+            mean, std = self.continuous_mean_std.unbind(dim=-1)
+            x_cont = (x_cont - mean) / std
+        normed_cont = self.norm(x_cont)
+        x = torch.cat((flat_categ,normed_cont), dim=-1)
+        return self.mlp(x)
