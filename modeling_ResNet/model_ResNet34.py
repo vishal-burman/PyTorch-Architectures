9aa12fffbf4814d3c69a5b0a111085f94df66b48
Vishal Burman <vishal.a.burman23@gmail.com>
2020-12-24 08:14:23 +0530

    model_ResNet34.py: Added basic structure

---
+++ modeling_ResNet/model_ResNet34.py
@@ -0,0 +1,18 @@
+def conv3x3(in_planes, out_planes, stride=1):
+    pass
+
+class BasicBlock(nn.Module):
+    def __init__(self):
+        pass
+    
+    def forward(self):
+        pass
+
+def ResNet(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+
