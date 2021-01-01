d359bb861ace53a16446946115f3c819fee97fa5
Vishal Burman <vishal.a.burman23@gmail.com>
2021-01-01 13:58:15 +0530

    model_large.py: Added layer initialization parameters

--- modeling_ResNet/model_large.py
+++ modeling_ResNet/model_large.py
@@ -56,5 +56,13 @@ class ResNet(nn.Module):
         self.avgpool = nn.AvgPool2d(7, stride=1)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, (2./n) ** .5)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
     def forward(self):
         pass
