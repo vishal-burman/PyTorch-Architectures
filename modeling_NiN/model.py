79736f5282c2bbaba16ffce8657e0370db4f1b32
Vishal Burman <vishal.a.burman23@gmail.com>
2021-01-18 22:58:17 +0530

    model.py: Network 1

--- modeling_NiN/model.py
+++ modeling_NiN/model.py
@@ -6,7 +6,18 @@ class NiN(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.num_classes = num_classes
-        self.classifier = nn.Sequential(**kwargs)
+        self.classifier = nn.Sequential(
+                nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(192, 160, kernel_size=1, stride=1, padding=0),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(160, 96, kernel_size=1, stride=1, padding=0),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+                nn.Dropout(0.5),
+
+
+                )
 
     def forward(self, x):
         pass
