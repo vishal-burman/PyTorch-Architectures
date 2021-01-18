f5f7168c70be5e8120522a3b75fc2bf650f48967
Vishal Burman <vishal.a.burman23@gmail.com>
2021-01-18 23:55:34 +0530

    model.py: Layer 1 dimension annotations

--- modeling_NiN/model.py
+++ modeling_NiN/model.py
@@ -7,12 +7,18 @@ class NiN(nn.Module):
         super().__init__()
         self.num_classes = num_classes
         self.classifier = nn.Sequential(
+                # Ref --> floor(H_in + 2*padding - dilation(kernel-1) -1)/stride + 1
+
+                # (32 + 2 * 2 -(5 - 1) - 1)/1 + 1 --> 32x32
                 nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
                 nn.ReLU(inplace=True),
+                # (32 + 2 * 0 -(1 - 1) - 1)/1 + 1 --> 32x32
                 nn.Conv2d(192, 160, kernel_size=1, stride=1, padding=0),
                 nn.ReLU(inplace=True),
+                # (32 + 2 * 0 -(1 - 1) - 1)/1 + 1 --> 32x32
                 nn.Conv2d(160, 96, kernel_size=1, stride=1, padding=0),
                 nn.ReLU(inplace=True),
+                # (32 + 2 * 1 -(3 - 1) - 1)/2 + 1 --> 16x16
                 nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
                 nn.Dropout(0.5),
 
