4007d1ae2ecaaccf442c9cbe35bdda7909eb1991
Vishal Burman <vishal.a.burman23@gmail.com>
2021-01-25 09:13:57 +0530

    FRN.py: Added structure of [FRN] module

---
+++ modeling_FRN/FRN.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class FilterResponseNormalization(nn.Module):
+    def __init__(self, num_features, eps=1e-6):
+        super().__init__()
+        pass
+
+    def forward(self, x):
+        pass
