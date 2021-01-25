c20c663377afbd22ca783947208127db26c53b98
Vishal Burman <vishal.a.burman23@gmail.com>
2021-01-25 09:19:16 +0530

    FRN.py: init function done

--- modeling_FRN/FRN.py
+++ modeling_FRN/FRN.py
@@ -4,7 +4,10 @@ import torch.nn as nn
 class FilterResponseNormalization(nn.Module):
     def __init__(self, num_features, eps=1e-6):
         super().__init__()
-        pass
+        self.register_parameter('beta', torch.nn.Parameter(torch.empty([1, num_features, 1, 1]).normal_()))
+        self.register_parameter('gamma', torch.nn.Parameter(torch.empty([1, num_features, 1, 1]).normal_()))
+        self.register_parameter('tau', torch.nn.Parameter(torch.empty([1, num_features, 1, 1]).normal_()))
+        self.eps = torch.Tensor([eps])
 
     def forward(self, x):
         pass
