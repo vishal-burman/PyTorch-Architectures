62732a34170f76b6c0a52f75864f46a5b040a067
Vishal Burman <vishal.a.burman23@gmail.com>
2021-01-25 07:38:53 +0530

    Added NiN batchnorm experiments

---
+++ research/modeling_NiN/NiN_bn_before_activation.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class NiN(nn.Module):
+    def __init__(self, num_classes):
+        super(NiN, self).__init__()
+        self.num_classes = num_classes
+        self.classifier = nn.Sequential(
+                nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2, bias=False),
+                nn.BatchNorm2d(192),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(192, 160, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(160),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(160,  96, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(96),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+                nn.Dropout(0.5),
+
+                nn.Conv2d(96, 192, kernel_size=5, stride=1, padding=2, bias=False),
+                nn.BatchNorm2d(192),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(192),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(192),
+                nn.ReLU(inplace=True),
+                nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
+                nn.Dropout(0.5),
+
+                nn.Conv2d(192, 192, kernel_size=3, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(192),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(192),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(192,  10, kernel_size=1, stride=1, padding=0),
+                nn.ReLU(inplace=True),
+                nn.AvgPool2d(kernel_size=8, stride=1, padding=0),
+
+                )
+
+    def forward(self, x):
+        x = self.classifier(x)
+        logits = x.view(x.size(0), self.num_classes)
+        probas = torch.softmax(logits, dim=1)
+        return logits, probas
