3741964534085287cfdd0a23371b5b3dc3c49d70
Vishal Burman <vishal.a.burman23@gmail.com>
2021-01-29 13:01:21 +0530

    model_ae.py: Dimension annotations

--- research/modeling_AutoEncoder/model_ae.py
+++ research/modeling_AutoEncoder/model_ae.py
@@ -15,12 +15,12 @@ class AutoEncoder(nn.Module):
         self.linear_2.weight.detach().normal_(0.0, 0.1)
         self.linear_2.bias.detach().zero_()
 
-    def forward(self, x):
-        x = x.view(x.size(0), -1)
-        encoded = self.linear_1(x)
-        encoded = F.leaky_relu(encoded)
+    def forward(self, x): # x ~ [bs, c, h, w]
+        x = x.view(x.size(0), -1) # x ~ [bs, c * h * w]
+        encoded = self.linear_1(x) # encoded ~ [bs, num_hidden_1]
+        encoded = F.leaky_relu(encoded) # encoded ~ [bs, num_hidden_1]
 
-        logits = self.linear_2(encoded)
-        decoded = torch.sigmoid(logits)
+        logits = self.linear_2(encoded) # logits ~ [bs, c * h * w]
+        decoded = torch.sigmoid(logits) # decoded ~ [bs, c * h * w]
         
         return decoded
