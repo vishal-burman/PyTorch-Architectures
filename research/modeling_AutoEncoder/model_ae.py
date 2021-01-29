52ce4f8ce274b191c7a7f7ae4da866f377ac6d93
Vishal Burman <vishal.a.burman23@gmail.com>
2021-01-29 12:35:01 +0530

    model_ae.py: Added [AutoEncoder] encoder layer

--- research/modeling_AutoEncoder/model_ae.py
+++ research/modeling_AutoEncoder/model_ae.py
@@ -3,8 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class AutoEncoder(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, num_features, num_hidden_1):
+        super().__init__()
+        # Encoder
+        self.linear_1 = nn.Linear(num_features, num_hidden_1)
+        self.linear_1.weight.detach().normal_(0.0, 0.1)
+        self.linear_1.bias.detach().zero_()
+
 
     def forward(self):
         pass
