725460e89d761106c3589c5d3275382f47fe3cab
Vishal Burman <vishal.a.burman23@gmail.com>
2021-01-30 11:32:35 +0530

    model_ae_conv.py: Added [AutoEncoder] structure

---
+++ research/modeling_AutoEncoder/model_ae_conv.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
