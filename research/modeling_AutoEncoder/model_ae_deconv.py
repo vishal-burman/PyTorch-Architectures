6d18f75f0ebddd14df8e0ad88ac16ec532d5dfac
Vishal Burman <vishal.a.burman23@gmail.com>
2021-02-10 23:59:23 +0530

    model_ae_deconv.py: Added structure of module

---
+++ research/modeling_AutoEncoder/model_ae_deconv.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvolutionalAutoEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
