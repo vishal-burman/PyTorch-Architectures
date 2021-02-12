c2bbd58651609f51966cfe3cdaf37a7c6996e3ec
Vishal Burman <vishal.a.burman23@gmail.com>
2021-02-12 20:31:02 +0530

    model_ae_nni.py: Added [ConvolutionalAutoEncoder] encoder layers

--- research/modeling_AutoEncoder/model_ae_nni.py
+++ research/modeling_AutoEncoder/model_ae_nni.py
@@ -5,8 +5,18 @@ import torch.nn.functional as F
 class ConvolutionalAutoEncoder(nn.Module):
     def __init__(self):
         super().__init__()
-        pass
+        self.conv_1 = nn.Conv2d(in_channels=1, out_channels=4, kernel_size=3, stride=1, padding=1) # 28x28x1 => 28x28x4
+        self.pool_1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0) # 28x28x4 => 14x14x4
+        self.conv_2 = nn.Conv2d(in_channels=4, out_channels=8, kernel_size=3, stride=1, padding=1) # 14x14x4 => 14x14x8
+        self.pool_2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0) # 14x14x8 => 7x7x8
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        # Encoder
+        x = self.conv_1(x)
+        x = F.leaky_relu(x)
+        x = self.pool_1(x)
+        x = self.conv_2(x)
+        x = F.leaky_relu(x)
+        x = self.pool_2(x)
+        return x
 
