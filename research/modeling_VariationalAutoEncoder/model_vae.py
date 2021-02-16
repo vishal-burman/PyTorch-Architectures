9ab8dec33e0dd3cdd06c8ec59565a474cc1996b1
Vishal Burman <vishal.a.burman23@gmail.com>
2021-02-16 16:30:33 +0530

    model_vae.py: Added torch nn structure

---
+++ research/modeling_VariationalAutoEncoder/model_vae.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class VariationalAutoEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def reparameterize(self):
+        pass
+
+    def encoder(self):
+        pass
+
+    def decoder(self):
+        pass
+
+    def forward(self):
+        pass
+
