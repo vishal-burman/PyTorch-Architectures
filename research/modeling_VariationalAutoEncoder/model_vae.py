0077ade9bc876f364f0dd3d60782eb41978ac30d
Vishal Burman <vishal.a.burman23@gmail.com>
2021-02-16 16:34:01 +0530

    model_vae.py: Added initializations

--- research/modeling_VariationalAutoEncoder/model_vae.py
+++ research/modeling_VariationalAutoEncoder/model_vae.py
@@ -3,9 +3,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class VariationalAutoEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, num_features, num_hidden_1, num_latent):
         super().__init__()
-        pass
+
+        ## Encoder
+        self.hidden_1 = nn.Linear(num_features, num_hidden_1)
+        self.z_mean = nn.Linear(num_hidden_1, num_latent)
+        self.z_log_var = nn.Linear(num_hidden_1, num_latent)
+
+        ## Decoder
+        self.linear_3 = nn.Linear(num_latent, num_hidden_1)
+        self.linear_4 = nn.Linear(num_hidden_1, num_features)
 
     def reparameterize(self):
         pass
