4d563b330af8627b3efeed67d807e97f30efe750
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-06 13:18:46 +0530

    model.py: Added [Word2Vec] initializations

--- modeling_Word2Vec/model.py
+++ modeling_Word2Vec/model.py
@@ -2,8 +2,17 @@ import torch
 import torch.nn as nn
 
 class Word2Vec(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, vocab_size, emb_size):
+        """
+        Arguments:
+        vocab_size --> Number of vocabulary words
+        emb_size --> Embedding dimension
+        """
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.emb_size = emb_size
+        self.W = nn.Embedding(vocab_size, emb_size)
+        self.WT = nn.Linear(emb_size, vocab_size)
 
     def forward(self):
         pass
