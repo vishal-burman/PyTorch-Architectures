512e9b19e7f8e28dcf780f517525e79256f1a744
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-07 21:24:26 +0530

    model.py: [FastText] added initializations

--- modeling_FastText/model.py
+++ modeling_FastText/model.py
@@ -3,8 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class FastText(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, vocab_size, embedding_size, hidden_size, output_size):
+        self.vocab_size = vocab_size
+        self.embedding_size = embedding_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.embedding = nn.Embedding(self.vocab_size, self.embedding_size)
+        self.lin_1 = nn.Linear(self.embedding_size, self.hidden_size)
+        self.lin_2 = nn.Linear(self.hidden_size, self.output_size)
 
     def forward(self):
         pass
