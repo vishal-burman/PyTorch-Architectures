707accf3c93751af7e1b5b29c81d51bca7995097
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-08 08:42:50 +0530

    model.py: [FastText] added forward pass

--- modeling_FastText/model.py
+++ modeling_FastText/model.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 
 class FastText(nn.Module):
     def __init__(self, vocab_size, embedding_size, hidden_size, output_size):
+        super().__init__()
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
         self.hidden_size = hidden_size
@@ -12,5 +13,9 @@ class FastText(nn.Module):
         self.lin_1 = nn.Linear(self.embedding_size, self.hidden_size)
         self.lin_2 = nn.Linear(self.hidden_size, self.output_size)
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        x = self.embedding(x)
+        x = F.avg_pool2d(x, kernel_size=(x.size(1), 1)).squeeze(1)
+        x = self.lin_1(x)
+        x = self.lin_2(x)
+        return x
