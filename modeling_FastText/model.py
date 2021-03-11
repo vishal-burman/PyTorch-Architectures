34a2daa8013bdf73d6000c1602ce7a5a7bfd69a6
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-11 16:06:14 +0530

    model.py: Add padding_idx to Embedding layer

--- modeling_FastText/model.py
+++ modeling_FastText/model.py
@@ -3,13 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class FastText(nn.Module):
-    def __init__(self, vocab_size, embedding_size, hidden_size, output_size):
+    def __init__(self, vocab_size, embedding_size, hidden_size, output_size, padding_idx):
         super().__init__()
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
         self.hidden_size = hidden_size
         self.output_size = output_size
-        self.embedding = nn.Embedding(self.vocab_size, self.embedding_size)
+        self.padding_idx = padding_idx
+        self.embedding = nn.Embedding(self.vocab_size, self.embedding_size, padding_idx=self.padding_idx)
         self.lin_1 = nn.Linear(self.embedding_size, self.hidden_size)
         self.lin_2 = nn.Linear(self.hidden_size, self.output_size)
 
