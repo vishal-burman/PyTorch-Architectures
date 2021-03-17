d072894d1f1eeba9d3b7b3f9090b23a4048e7075
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-17 18:02:18 +0530

    model.py: Added [TextCNN] initializations

--- modeling_TextCNN/model.py
+++ modeling_TextCNN/model.py
@@ -3,9 +3,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class TextCNN(nn.Module):
-    def __init__(self):
+    def __init__(self, num_filters, filter_sizes, vocab_size, embedding_size):
         super().__init__()
-        pass
+        self.num_filters = num_filters
+        self.filter_sizes = filter_sizes
+        self.vocab_size = vocab_size
+        self.embedding_size = embedding_size
+        self.num_filters_total = self.num_filters * len(filter_sizes)
+        self.W = nn.Embedding(self.vocab_size, self.embedding_size)
+        self.Weight = nn.Linear(self.num_filters_total, num_classes, bias=False)
+        self.Bias = nn.Parameter(torch.ones([num_classes]))
+        self.filter_list = nn.ModuleList([nn.Conv2d(1, num_filters, (size, embedding_size)) for size in filter_sizes])
 
     def forward(self):
         pass
