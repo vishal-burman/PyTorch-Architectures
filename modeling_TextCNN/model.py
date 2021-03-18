bc12723bb81acf47eaa4ad5115ea9c1276f77b86
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-18 12:15:52 +0530

    model.py: Added [TextCNN] forward function

--- modeling_TextCNN/model.py
+++ modeling_TextCNN/model.py
@@ -3,17 +3,32 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class TextCNN(nn.Module):
-    def __init__(self, num_filters, filter_sizes, vocab_size, embedding_size):
+    def __init__(self, num_filters, filter_sizes, vocab_size, embedding_size, sequence_length, num_classes=2):
         super().__init__()
         self.num_filters = num_filters
         self.filter_sizes = filter_sizes
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
-        self.num_filters_total = self.num_filters * len(filter_sizes)
+        self.sequence_length = sequence_length
+        self.num_classes = num_classes
+        self.num_filters_total = self.num_filters * len(self.filter_sizes)
         self.W = nn.Embedding(self.vocab_size, self.embedding_size)
-        self.Weight = nn.Linear(self.num_filters_total, num_classes, bias=False)
-        self.Bias = nn.Parameter(torch.ones([num_classes]))
-        self.filter_list = nn.ModuleList([nn.Conv2d(1, num_filters, (size, embedding_size)) for size in filter_sizes])
+        self.Weight = nn.Linear(self.num_filters_total, self.num_classes, bias=False)
+        self.Bias = nn.Parameter(torch.ones([self.num_classes]))
+        self.filter_list = nn.ModuleList([nn.Conv2d(1, self.num_filters, (size, self.embedding_size)) for size in self.filter_sizes])
 
-    def forward(self):
-        pass
+    def forward(self, x): # x ~ [batch_size, max_seq_len]
+        x = self.W(x)
+        x = x.unsqueeze(1)
+        
+        pooled_outputs = []
+        for i, conv in enumerate(self.filter_list):
+            h = F.relu(conv(x))
+            mp = nn.MaxPool2d((self.sequence_length - self.filter_sizes[i] + 1, 1))
+            pooled = mp(h).permute(0, 3, 2, 1)
+            pooled_outputs.append(pooled)
+
+        h_pool = torch.cat(pooled_outputs, len(self.filter_sizes))
+        h_pool_flat = h_pool.reshape(h_pool.size(0), -1)
+        logits = self.Weight(h_pool_flat) + self.Bias
+        return logits
