31d7d53f86d6aea0c58ef5f72c0ff1f2587716a9
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-18 12:26:45 +0530

    model.py: Added [TextCNN] dimension annotations

--- modeling_TextCNN/model.py
+++ modeling_TextCNN/model.py
@@ -18,17 +18,18 @@ class TextCNN(nn.Module):
         self.filter_list = nn.ModuleList([nn.Conv2d(1, self.num_filters, (size, self.embedding_size)) for size in self.filter_sizes])
 
     def forward(self, x): # x ~ [batch_size, max_seq_len]
-        x = self.W(x)
-        x = x.unsqueeze(1)
+        x = self.W(x) # x ~ [batch_size, max_seq_len, embedding_size]
+        x = x.unsqueeze(1) # x ~ [batch_size, 1, max_seq_len, embedding_size]
         
         pooled_outputs = []
         for i, conv in enumerate(self.filter_list):
-            h = F.relu(conv(x))
+            h = F.relu(conv(x)) # h ~ [batch_size, num_filters, self.filter_size, 1] 
             mp = nn.MaxPool2d((self.sequence_length - self.filter_sizes[i] + 1, 1))
-            pooled = mp(h).permute(0, 3, 2, 1)
+            pooled = mp(h).permute(0, 3, 2, 1) # pooled ~ [batch_size, 1, 1, 3]
             pooled_outputs.append(pooled)
 
-        h_pool = torch.cat(pooled_outputs, len(self.filter_sizes))
-        h_pool_flat = h_pool.reshape(h_pool.size(0), -1)
-        logits = self.Weight(h_pool_flat) + self.Bias
+        h_pool = torch.cat(pooled_outputs, len(self.filter_sizes)) # h_pool ~ [batch_size, 1, 1, 3 * 3]
+        h_pool_flat = h_pool.reshape(h_pool.size(0), -1) # h_pool_flat ~ [batch_size, 1 * 1 * 3 * 3]
+        logits = self.Weight(h_pool_flat) + self.Bias # logits ~ [batch_size, num_classes]
         return logits
+        """
