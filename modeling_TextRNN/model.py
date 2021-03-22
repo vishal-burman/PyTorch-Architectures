83d7f48aefc769a98075b357f3d4d641ba1f3d36
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-23 00:29:56 +0530

    model.py: Added structure [TextRNN]

---
+++ modeling_TextRNN/model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class TextRNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
