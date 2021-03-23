9d5c2168a1a884c3844b1cc20ebfb420c58d8f5d
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-23 12:48:31 +0530

    model.py: Added Dimension Annotations [TextRNN]

--- modeling_TextRNN/model.py
+++ modeling_TextRNN/model.py
@@ -11,10 +11,10 @@ class TextRNN(nn.Module):
         self.W = nn.Linear(self.hidden_size, self.vocab_size, bias=False)
         self.B = nn.Parameter(torch.ones([self.vocab_size]))
 
-    def forward(self, x):
-        x = x.transpose(0, 1)
+    def forward(self, x): # x ~ [batch_size, seq_length, vocab_size]
+        x = x.transpose(0, 1) # x ~ [seq_length, batch_size, vocab_size]
         outputs, hidden = self.rnn(x)
-        outputs = outputs[-1]
-        logits = self.W(outputs) + self.B
+        outputs = outputs[-1] # outputs ~ [batch_size, vocab_size]
+        logits = self.W(outputs) + self.B # logits ~ [batch_size, vocab_size]
         return logits
 
