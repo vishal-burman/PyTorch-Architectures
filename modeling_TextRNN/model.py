47da96f823a23bdd2f67852cfe6edd342aa39c1d
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-23 12:24:43 +0530

    model.py: Added [TextRNN] initializations

--- modeling_TextRNN/model.py
+++ modeling_TextRNN/model.py
@@ -3,9 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class TextRNN(nn.Module):
-    def __init__(self):
+    def __init__(self, vocab_size, hidden_size):
         super().__init__()
-        pass
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.rnn = nn.RNN(input_size=vocab_size, hidden_size=self.hidden_size)
+        self.W = nn.Linear(self.hidden_size, self.vocab_size, bias=False)
+        self.B = nn.Parameter(torch.ones([self.vocab_size]))
 
     def forward(self):
         pass
