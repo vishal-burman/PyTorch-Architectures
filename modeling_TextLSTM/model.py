e325158ced12d7a7f72f5a2527482d73c5746f1e
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-25 21:53:36 +0530

    model.py: Added [TextLSTM] initializations

--- modeling_TextLSTM/model.py
+++ modeling_TextLSTM/model.py
@@ -3,9 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class TextLSTM(nn.Module):
-    def __init__(self):
+    def __init__(self, vocab_size, hidden_size):
         super().__init__()
-        pass
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(input_size=self.vocab_size, hidden_size=self.hidden_size)
+        self.W = nn.Linear(self.hidden_size, self.vocab_size, bias=False)
+        self.b = nn.Parameter(torch.zeros([self.vocab_size]))
 
     def forward(self):
         pass
