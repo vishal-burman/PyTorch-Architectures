26b38759b7ce8909f78998211be16fa488dc7683
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-25 22:50:11 +0530

    model.py: Fixed [TextLSTM] initializations

--- modeling_TextLSTM/model.py
+++ modeling_TextLSTM/model.py
@@ -3,14 +3,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class TextLSTM(nn.Module):
-    def __init__(self, vocab_size, hidden_size):
+    def __init__(self, vocab_size, embedding_size, hidden_size, padding_idx=None):
         super().__init__()
         self.vocab_size = vocab_size
         self.hidden_size = hidden_size
-        self.lstm = nn.LSTM(input_size=self.vocab_size, hidden_size=self.hidden_size)
+        self.embedding_size = embedding_size
+        self.padding_idx = padding_idx
+        if self.padding_idx is not None:
+            self.embedding = nn.Embedding(self.vocab_size, self.embedding_size, padding_idx=self.padding_idx)
+        else:
+            self.embedding = nn.Embedding(self.vocab_size, self.embedding_size)
+        self.lstm = nn.LSTM(input_size=self.embedding_size, hidden_size=self.hidden_size)
         self.W = nn.Linear(self.hidden_size, self.vocab_size, bias=False)
         self.b = nn.Parameter(torch.zeros([self.vocab_size]))
 
-    def forward(self):
+    def forward(self, x):
         pass
 
