fe4be39ea4d3593aa2f234566b1362321243c18d
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-26 08:46:08 +0530

    model.py: Added forward pass [TextBiLSTM]

--- modeling_TextBiLSTM/model.py
+++ modeling_TextBiLSTM/model.py
@@ -3,10 +3,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class TextBiLSTM(nn.Module):
-    def __init__(self, vocab_size, embedding_size, padding_idx=None):
+    def __init__(self, vocab_size, embedding_size, hidden_size, padding_idx=None):
         super().__init__()
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
+        self.hidden_size = hidden_size
+        self.padding_idx = padding_idx
 
         if self.padding_idx is not None:
             self.embedding = nn.Embedding(self.vocab_size, self.embedding_size, padding_idx=self.padding_idx)
@@ -17,6 +19,10 @@ class TextBiLSTM(nn.Module):
         self.W = nn.Linear(self.hidden_size * 2, self.vocab_size, bias=False)
         self.b = nn.Parameter(torch.ones([self.vocab_size]))
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        x = self.embedding(x).transpose(0, 1)
+        outputs, (_, _) = self.lstm(x)
+        outputs = outputs[-1]
+        logits = self.W(outputs) + self.b
+        return logits
 
