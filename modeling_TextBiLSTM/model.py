4eb7afacc30b834fa1ab3cc59dd35dd54e5dedb5
Vishal Burman <vishal.a.burman23@gmail.com>
2021-03-26 08:39:39 +0530

    model.py: Added [TextBiLSTM] model initializations

--- modeling_TextBiLSTM/model.py
+++ modeling_TextBiLSTM/model.py
@@ -3,9 +3,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class TextBiLSTM(nn.Module):
-    def __init__(self):
+    def __init__(self, vocab_size, embedding_size, padding_idx=None):
         super().__init__()
-        pass
+        self.vocab_size = vocab_size
+        self.embedding_size = embedding_size
+
+        if self.padding_idx is not None:
+            self.embedding = nn.Embedding(self.vocab_size, self.embedding_size, padding_idx=self.padding_idx)
+        else:
+            self.embedding = nn.Embedding(self.vocab_size, self.embedding_size)
+
+        self.lstm = nn.LSTM(self.embedding_size, self.hidden_size, bidirectional=True)
+        self.W = nn.Linear(self.hidden_size * 2, self.vocab_size, bias=False)
+        self.b = nn.Parameter(torch.ones([self.vocab_size]))
 
     def forward(self):
         pass
