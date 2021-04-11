d1fa158ac50f5913095ccb7b73e74991fe7139d7
Vishal Burman <vishal.a.burman23@gmail.com>
2021-04-11 08:18:55 +0530

    model.py: Added [Seq2Seq] structure

---
+++ modeling_Seq2Seq/model.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+class Seq2Seq(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
