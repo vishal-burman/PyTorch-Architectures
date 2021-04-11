9656b1913b1db6812bd7c6e52d3b78c184f817e4
Vishal Burman <vishal.a.burman23@gmail.com>
2021-04-11 08:40:09 +0530

    model.py: Added [Encoder] initializations

--- modeling_Seq2Seq/model.py
+++ modeling_Seq2Seq/model.py
@@ -3,9 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Encoder(nn.Module):
-    def __init__(self):
+    def __init__(self, input_dim, hidden_dim, num_layers, p_drop=0.1):
         super().__init__()
-        pass
+        self.input_dim = input_dim
+        self.emb_dim = emb_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.p_drop = p_drop
+        self.embedding = nn.Embedding(self.input_dim, self.emb_dim)
+        self.rnn = nn.LSTM(self.emb_dim, self.hidden_dim, self.num_layers, dropout=self.p_drop)
+        self.dropout = nn.Dropout(self.p_drop)
 
     def forward(self):
         pass
