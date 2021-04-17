1476cdd8a4b07f33e79d614c4f64f20ab1fe86f5
Vishal Burman <vishal.a.burman23@gmail.com>
2021-04-17 18:05:18 +0530

    model.py: Added dimension annotations

--- modeling_Seq2Seq/model.py
+++ modeling_Seq2Seq/model.py
@@ -1,3 +1,4 @@
+import random
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -14,10 +15,10 @@ class Encoder(nn.Module):
         self.rnn = nn.LSTM(self.emb_dim, self.hidden_dim, self.num_layers, dropout=self.p_drop)
         self.dropout = nn.Dropout(self.p_drop)
 
-    def forward(self, src):
-        embedded = self.embedding(src)
-        embedded = self.dropout(embedded)
-        outputs, (hidden, cell) = self.rnn(embedded)
+    def forward(self, src): # src ~ [max_len, bs]
+        embedded = self.embedding(src) # embedded ~ [max_len, bs, embedding_size]
+        embedded = self.dropout(embedded) # embedded ~ [max_len, bs, embedding_size]
+        outputs, (hidden, cell) = self.rnn(embedded) # hidden, cell ~ [num_layers, bs, hidden_size]
         return hidden, cell
 
 class Decoder(nn.Module):
@@ -33,12 +34,12 @@ class Decoder(nn.Module):
         self.fc_out = nn.Linear(self.hidden_dim, self.output_dim)
         self.dropout = nn.Dropout(self.p_drop)
 
-    def forward(self, input, hidden, cell):
-        input = input.unsqueeze(0)
-        embedded = self.embedding(input)
-        embedded = self.dropout(embedded)
-        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
-        prediction = self.fc_out(output.squeeze(0))
+    def forward(self, input, hidden, cell): # input ~ [bs] | hidden, cell ~ [num_layers, bs, hidden_size]
+        input = input.unsqueeze(0) # input ~ [1, bs]
+        embedded = self.embedding(input) # embedded ~ [1, bs, embedding_size]
+        embedded = self.dropout(embedded) # embedded ~ [1, bs, embedding_size]
+        output, (hidden, cell) = self.rnn(embedded, (hidden, cell)) # hidden, cell ~ [num_layers, bs, hidden_size] | output ~ [1, bs, hidden_size]
+        prediction = self.fc_out(output.squeeze(0)) # prediction ~ [bs, trg_vocab_size]
         return prediction, hidden, cell
 
 class Seq2Seq(nn.Module):
@@ -47,18 +48,18 @@ class Seq2Seq(nn.Module):
         self.encoder = encoder
         self.decoder = decoder
 
-    def forward(self, src, trg, teacher_forcing_ratio=0.5):
-        batch_size = trg.size(1)
-        trg_len = trg.size(0)
-        trg_vocab_size = self.decoder.output_dim
+    def forward(self, src, trg, teacher_forcing_ratio=0.5): # src ~ [max_len, bs] | trg ~ [max_len, bs]
+        batch_size = trg.size(1) # batch_size ~ bs
+        trg_len = trg.size(0) # trg_len ~ max_len
+        trg_vocab_size = self.decoder.output_dim # trg_vocab_size
         
-        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(src.device)
-        hidden, cell = self.encoder(src)
-        input = trg[0, :] # First input to the decoder is <sos> token
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(src.device) # outputs ~ [max_len, bs, trg_vocab_size]
+        hidden, cell = self.encoder(src) # hidden, cell ~ [num_layers, bs, hidden_size]
+        input = trg[0, :] # First input to the decoder is <sos> token # input ~ [bs]
         for t in range(1, trg_len):
-            output, hidden, cell = self.decoder(input, hidden, cell)
-            outputs[t] = output
+            output, hidden, cell = self.decoder(input, hidden, cell) # output ~ [bs, trg_vocab_size] | hidden, cell ~ [num_layers, bs, hidden_size]
+            outputs[t] = output # outputs ~ [max_len, bs, trg_vocab_size]
             teacher_force = random.random() < teacher_forcing_ratio
             top1 = output.argmax(1)
             input = trg[t] if teacher_force else top1
-        return outputs
+        return outputs # outputs ~ [max_len, bs, trg_vocab_size]
