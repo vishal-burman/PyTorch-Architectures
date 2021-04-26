0b26d27169c7018624df3b9fe5702f369380ab91
Vishal Burman <vishal.a.burman23@gmail.com>
2021-04-26 22:49:36 +0530

    model.py: Added [Seq2SeqPR] structure

--- modeling_Seq2SeqPR/model.py
+++ modeling_Seq2SeqPR/model.py
@@ -16,3 +16,12 @@ class Decoder(nn.Module):
 
     def forward(self):
         pass
+
+class Seq2SeqPR(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
