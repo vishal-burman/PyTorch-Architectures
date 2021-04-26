bf7d61f9387390506211013118345d2b51a4c298
Vishal Burman <vishal.a.burman23@gmail.com>
2021-04-26 22:53:00 +0530

    model.py: Added [Encoder] initializations

--- modeling_Seq2SeqPR/model.py
+++ modeling_Seq2SeqPR/model.py
@@ -2,9 +2,15 @@ import torch
 import torch.nn as nn
 
 class Encoder(nn.Module):
-    def __init__(self):
+    def __init__(self, input_dim, emb_dim, hidden_dim, p_drop=0.1):
         super().__init__()
-        pass
+        self.input_dim = input_dim
+        self.emb_dim = emb_dim
+        self.hidden_dim = hidden_dim
+        self.p_drop = p_drop
+        self.embedding = nn.Embedding(self.input_dim, self.emb_dim)
+        self.rnn = nn.GRU(self.emb_dim, self.hidden_dim)
+        self.dropout = nn.Dropout(self.p_drop)
 
     def forward(self):
         pass
