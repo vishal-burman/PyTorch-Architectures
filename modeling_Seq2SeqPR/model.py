98762922677b678c6ba17ed37e7ba9b14008383f
Vishal Burman <vishal.a.burman23@gmail.com>
2021-04-27 11:50:59 +0530

    model.py: Added [Seq2SeqPR] forward pass

--- modeling_Seq2SeqPR/model.py
+++ modeling_Seq2SeqPR/model.py
@@ -46,6 +46,18 @@ class Seq2SeqPR(nn.Module):
         self.encoder = encoder
         self.decoder = decoder
 
-    def forward(self):
-        pass
-
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        batch_size = trg.size(1)
+        trg_len = trg.size(0)
+        trg_vocab_size = self.decoder.output_dim
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(src.device)
+        context = self.encoder(src)
+        hidden = context
+        input = [0, :]
+        for t in range(1, trg_len):
+            output, hidden = self.decoder(input, hidden, context)
+            outputs[t] = output
+            teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.argmax(1)
+            input = trg[t] if teacher_force else top1
+        return outputs
