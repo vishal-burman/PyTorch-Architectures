31f5e2e9720c465048f026a6f46ed4e30540c4a7
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-03 19:28:26 +0530

    model.py: Added fwd & bwd pos_seq

--- modeling_XLNet/model.py
+++ modeling_XLNet/model.py
@@ -19,6 +19,14 @@ class XLNetModel(nn.Module):
         self.layer = nn.ModuleList([XLNetLayer(config) for _ in config.n_layers])
         self.dropout = nn.Dropout(config.dropout)
 
+    def relative_positional_encoding(self, qlen, klen, bsz=None): # qlen = klen
+        freq_seq = torch.arange(0, self.d_model, 2.0, dtype=torch.float) # freq_seq = [0, 2.0, 4.0 ... ]
+        inv_seq = 1 / torch.pow(10000, (freq_seq / self.d_model))
+        beg, end = klen, -qlen
+        fwd_pos_seq = torch.arange(beg, end, -1.0, dtype=torch.float)
+        bwd_pos_seq = torch.arange(-beg, -end, 1.0, dtype=torch.float)
+
+
     def forward(self, input_ids=None, attention_mask=None): # input_ids, attention_mask ~ [batch_size, max_len]
         input_ids = input_ids.transpose(0, 1).contiguous() # input_ids ~ [max_len, batch_size]
         qlen, bs = input_ids.size(0), input_ids.size(1)
@@ -34,7 +42,7 @@ class XLNetModel(nn.Module):
         non_tgt_mask = -torch.eye(qlen).to(attn_mask) # non_tgt_mask ~ [max_len, max_len]
         non_tgt_mask = ((attn_mask + non_tgt_mask[:, :, None, None]) > 0).to(attn_mask) # non_tgt_mask ~ [max_len, max_len, batch_size, 1]
         word_emb_k = self.word_embedding(input_ids) # word_emb_k ~ [max_len, batch_size, emb_size]
-        output_h = self.dropout(word_emb_k)
+        output_h = self.dropout(word_emb_k) # output_h ~ [max_len, batch_size, emb_size]
         output_g = None
         seg_mat = None
         pos_emb = self.relative_positional_encoding(qlen, klen, bs=bs)
