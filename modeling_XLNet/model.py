ea50862e21bd3888843195136840c049b50a98dd
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-10 20:53:46 +0530

    model.py: Added nn.LayerNorm init weights

--- modeling_XLNet/model.py
+++ modeling_XLNet/model.py
@@ -15,6 +15,9 @@ class InitModule(nn.Module):
             module.weight.data.normal_(mean=0.0, std=0.02)
             if module.padding_idx is not None:
                 module.weight.data[module.padding_idx].zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
 
     def init_weights(self):
         self.apply(self._init_weights)
