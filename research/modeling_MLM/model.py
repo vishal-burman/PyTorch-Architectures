c8c601a98544c60fd5d2243965ce5eecca44848f
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-11 11:56:56 +0530

    model.py: Added get_mask_subset_with_prob function

--- research/modeling_MLM/model.py
+++ research/modeling_MLM/model.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,6 +13,14 @@ def get_mask_subset_with_prob(mask, prob): # mask ~ [batch_size, seq_len]
     batch, seq_len, device = *mask.shape, mask.device
     max_masked = math.ceil(prob * seq_len) 
     num_tokens = mask.sum(dim=-1, keepdim=True) # num_tokens ~ [batch_size, 1]
+    mask_excess = (mask.cumsum(dim=-1) > (num_tokens * prob).ceil()) # max_excess ~ [batch_size, max_len]
+    mask_excess = mask_excess[:, :max_masked] # mask_excess ~ [batch_size, max_masked]
+    rand = torch.rand((batch, seq_len), device=device).masked_fill(~mask, 1e-9)
+    _, sampled_indices = rand.topk(max_masked, dim=-1)
+    sampled_indices = (sampled_indices + 1).masked_fill_(mask_excess, 0)
+    new_mask = torch.zeros((batch, seq_len + 1), device=device)
+    new_mask.scatter_(-1, sampled_indices, 1)
+    return new_mask[:, 1:].bool()
 
 class MLM(nn.Module):
     def __init__(self, mask_prob=0.15, pad_token_id=0, mask_token_id=2, mask_ignore_token_ids=[]):
