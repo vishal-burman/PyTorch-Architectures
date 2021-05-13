model.py: Bug fix for logits calculation

research/modeling_MLM/model.py
@@ -44,6 +44,6 @@ class MLM(nn.Module):
         replace_prob = prob_mask_like(input_ids, self.replace_prob) # replace_prob ~ [batch_size, max_len]
         masked_input = masked_input.masked_fill_(mask * replace_prob, self.mask_token_id) # masked_input ~ [batch_size, max_len]
         labels = input_ids.masked_fill(~mask, self.pad_token_id) # labels ~ [batch_size, max_len]
-        logits = self.transformer(masked_input, **kwargs)
+        logits = self.transformer(masked_input, **kwargs).last_hidden_state
         mlm_loss = F.cross_entropy(logits.transpose(1, 2), labels, ignore_index=self.pad_token_id)
         return mlm_loss