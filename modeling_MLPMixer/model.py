1267d6f79aa7854f5833db38dbf729336b31012f
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-15 13:03:54 +0530

    model.py: Added [FeedForwardLayer] initializations

--- modeling_MLPMixer/model.py
+++ modeling_MLPMixer/model.py
@@ -11,9 +11,14 @@ class PreNormResidual(nn.Module):
         return self.fn(self.norm(x)) + x
 
 class FeedForwardLayer(nn.Module):
-    def __init__(self):
+    def __init__(self, dim, expansion_factor=4, p_drop=0.):
         super().__init__()
-        pass
+        self.dim = dim
+        self.expansion_factor = expansion_factor
+        self.dropout = nn.Dropout(p_drop)
+        self.gelu = nn.GELU()
+        self.lin_1 = nn.Linear(self.dim, self.dim * self.expansion_factor)
+        self.lin_2 = nn.Linear(self.dim * self.expansion_factor, self.dim)
 
     def forward(self):
         pass
