5a2c756d6f276667c7a503548685dbf282e3ebe5
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-15 12:54:27 +0530

    model.py: Added [PreNormResidual] forward pass

--- modeling_MLPMixer/model.py
+++ modeling_MLPMixer/model.py
@@ -2,10 +2,10 @@ import torch.nn as nn
 from functools import partial
 
 class PreNormResidual(nn.Module):
-    def __init__(self):
+    def __init__(self, dim, fn):
         super().__init__()
-        pass
+        self.fn = fn
+        self.norm = nn.LayerNorm(dim)
 
-    def forward(self):
-        pass
-
+    def forward(self, x):
+        return self.fn(self.norm(x)) + x
