fbecbc7446f2e2fc09b4646080292b94a058641e
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-15 12:51:43 +0530

    model.py: Added [PreNormResidual] structure

---
+++ modeling_MLPMixer/model.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+from functools import partial
+
+class PreNormResidual(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
