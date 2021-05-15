4720ef8b081dfc8d3917ae9aa828417c848f8490
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-15 13:14:53 +0530

    model.py: Added adaptive dense layers

--- modeling_MLPMixer/model.py
+++ modeling_MLPMixer/model.py
@@ -1,3 +1,4 @@
+import pdb
 import torch.nn as nn
 from functools import partial
 
@@ -11,14 +12,19 @@ class PreNormResidual(nn.Module):
         return self.fn(self.norm(x)) + x
 
 class FeedForwardLayer(nn.Module):
-    def __init__(self, dim, expansion_factor=4, p_drop=0.):
+    def __init__(self, dim, expansion_factor=4, p_drop=0., dense=nn.Linear):
         super().__init__()
         self.dim = dim
         self.expansion_factor = expansion_factor
         self.dropout = nn.Dropout(p_drop)
         self.gelu = nn.GELU()
-        self.lin_1 = nn.Linear(self.dim, self.dim * self.expansion_factor)
-        self.lin_2 = nn.Linear(self.dim * self.expansion_factor, self.dim)
+        self.dense = dense
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        pdb.set_trace()
+        x = self.dense(self.dim, self.dim * self.expansion_factor)(x)
+        x = self.gelu(x)
+        x = self.dropout(x)
+        x = self.dense(self.dim * self.expansion_factor, self.dim)(x)
+        x = self.dropout(x)
+        return x
