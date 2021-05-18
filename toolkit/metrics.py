779de89345b2990fa220f64430d099c9024c859c
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-18 22:36:14 +0530

    metrics.py: Added compute_accuracy function

---
+++ toolkit/metrics.py
@@ -0,0 +1,28 @@
+import torch
+
+def compute_accuracy(model, data_loader, device):
+    correct, total = 0, 0
+    with torch.set_grad_enabled(False):
+        for sample in data_loader:
+            if sample['input_ids'].dim() == 3:
+                assert sample['input_ids'].shape == sample['attention_mask'].shape, 'input_ids and attention_mask shape do not match'
+                input_ids = sample['input_ids'].squeeze(1).to(device)
+                attention_mask = sample['attention_mask'].squeeze(1).to(device)
+                labels = sample['labels'].to(device)
+            else:
+                assert sample['input_ids'].shape == sample['attention_mask'].shape, 'input_ids and attention_mask shape do not match'
+                input_ids = sample['input_ids'].to(device)
+                attention_mask = sample['attention_mask'].to(device)
+
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            if type(outputs) is tuple:
+                logits = outputs[0]
+                loss = outputs[1]
+            else:
+                logits = outputs
+
+            prob = F.softmax(logits, dim=-1)
+            _, preds = torch.max(prob, dim=1)
+            correct += (preds == labels).sum()
+            total += labels.size(0)
+    return correct.float() / total * 100
