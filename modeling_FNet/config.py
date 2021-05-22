b11fbf3a9dbbe09db25bc3934d51db0be0e570e2
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-23 00:29:49 +0530

    config.py: Added config parameters

--- modeling_FNet/config.py
+++ modeling_FNet/config.py
@@ -1,13 +1,19 @@
 class FNetConfig:
     def __init__(self,
             vocab_size=30522,
-            embed_dim=768,
+            dim=768,
+            expanded_dim=2048,
             max_position_embed=512,
             padding_idx=0,
-            p_drop=0.1):
+            p_drop=0.1,
+            depth=8,
+            eps=1e-12,):
 
         self.vocab_size = vocab_size
-        self.embed_dim = embed_dim
+        self.dim = dim
+        self.expanded_dim = expanded_dim
         self.max_position_embed = max_position_embed
         self.padding_idx = padding_idx
         self.p_drop = p_drop
+        self.depth = depth
+        self.eps = eps
