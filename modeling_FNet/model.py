6a535f4f64b57b4c4ba65913e02236a50cd2925c
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-22 20:09:49 +0530

    model.py: Added [Embeddings] initializations

--- modeling_FNet/model.py
+++ modeling_FNet/model.py
@@ -3,8 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Embeddings(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, vocab_size, embed_dim, max_position_embed, padding_idx=None, p_drop=0.1):
+        self.vocab_size = vocab_size
+        self.embed_dim = embed_dim
+        self.max_position_embed = max_position_embed
+        self.padding_idx = padding_idx
+        self.word_embeddings = nn.Embedding(self.vocab_size, self.embed_dim, padding_idx=self.padding_idx)
+        self.layer_norm = nn.LayerNorm(self.embed_dim, eps=1e-12)
+        self.dropout = nn.Dropout(self.p_drop)
     
     def forward(self):
         pass
