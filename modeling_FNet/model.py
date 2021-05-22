5855f8235b4886808a7633c71ddab449a616e100
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-22 20:03:38 +0530

    model.py: Added [Embeddings] structure

---
+++ modeling_FNet/model.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Embeddings(nn.Module):
+    def __init__(self):
+        pass
+    
+    def forward(self):
+        pass
