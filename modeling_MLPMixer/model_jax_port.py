9a3160f5af038fbf8208bffd0315ce75166eba68
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-24 10:56:50 +0530

    model_jax_port.py: Added [MLPMixer] structure

---
+++ modeling_MLPMixer/model_jax_port.py
@@ -0,0 +1,13 @@
+# This is a straight port from JAX/Flax code to PyTorch code
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MLPMixer(nn.Module):
+    def __init__(self, num_classes, num_blocks, patch_size, hidden_dim, tokens_mlp_dim, channels_mlp_dim):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
