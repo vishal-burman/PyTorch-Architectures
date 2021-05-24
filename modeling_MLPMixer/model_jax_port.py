ae9a0af0623801588c39f7915bd1134211370e71
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-24 12:26:55 +0530

    model_jax_port.py: Bug fixes and [MixerBlock] forward pass added

--- modeling_MLPMixer/model_jax_port.py
+++ modeling_MLPMixer/model_jax_port.py
@@ -1,8 +1,6 @@
 # This is a straight port from JAX/Flax code to PyTorch code as mentioned in the paper
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 class MLPBlock(nn.Module):
     def __init__(self, input_dim, mlp_dim):
@@ -15,18 +13,22 @@ class MLPBlock(nn.Module):
         x = self.dense_1(x)
         x = self.gelu(x)
         x = self.dense_2(x)
-        return y
+        return x
 
 class MixerBlock(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.layer_norm = nn.LayerNorm(config.hidden_dim)
         self.token_mixing = MLPBlock(config.patch_size ** 2, config.tokens_mlp_dim)
+        self.channel_mixing = MLPBlock(config.hidden_dim, config.channels_mlp_dim)
 
     def forward(self, x):
         y = self.layer_norm(x)
         y = y.transpose(1, 2)
-        y = self.token_mixing(y)
+        y = self.token_mixing(y).transpose(1, 2)
+        x = x + y
+        y = self.layer_norm(x)
+        x = x + self.channel_mixing(y)
         return x
 
 class MLPMixer(nn.Module):
@@ -42,4 +44,6 @@ class MLPMixer(nn.Module):
 
     def forward(self, x): # x ~ [batch_size, num_channels, height, width]
         x = self.conv(x).flatten(2).transpose(1, 2)
+        for layer in self.layers:
+            x = layer(x)
         return x
