5a9842bec1fc47b046b5dadb607cf6ab36e30550
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-25 13:52:58 +0530

    custom_dataset.py: Added [DataLoaderCIFAR10Classification]

--- toolkit/custom_dataset.py
+++ toolkit/custom_dataset.py
@@ -52,3 +52,11 @@ class DataLoaderTextClassification:
 
     def return_dataloader(self, batch_size=4, shuffle=False):
         return DataLoader(self.dataset, batch_size, shuffle=shuffle, collate_fn=self.dataset.collate_fn)
+
+class DataLoaderCIFAR10Classification:
+    def __init__(self, train=True):
+        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        self.dataset = torchvision.datasets.CIFAR10(root='./data', train=train, download=True, transform=self.transform)
+
+    def return_dataloader(self, batch_size=4, shuffle=False):
+        return DataLoader(self.dataset, batch_size=batch_size, shuffle=shuffle)
