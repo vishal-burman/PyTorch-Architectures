ca9f1eb4fe5a3cfea70e35379a64bd056806490c
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-26 07:02:16 +0530

    custom_dataset_cv.py: Added resize transform

--- toolkit/custom_dataset_cv.py
+++ toolkit/custom_dataset_cv.py
@@ -3,8 +3,8 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 
 class DataLoaderCIFAR10Classification:
-    def __init__(self, train=True):
-        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    def __init__(self, resize=256, train=True):
+        self.transform = transforms.Compose([transforms.Resize(resize), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
         self.dataset = torchvision.datasets.CIFAR10(root='./data', train=train, download=True, transform=self.transform)
 
     def return_dataloader(self, batch_size=4, shuffle=False):
