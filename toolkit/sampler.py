3a5eb892ae0dbb7c46d47497009ec965b790ae6e
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-28 12:36:42 +0530

    sampler.py: Added [SortishSampler] structure

---
+++ toolkit/sampler.py
@@ -0,0 +1,11 @@
+from torch.utils.data import Sampler
+
+class SortishSampler(Sampler):
+    def __init__(self, data, batch_size, shuffle=False):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __iter__(self):
+        pass
