7ee57a13c052445fd074b1123e100d297e3bd670
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-28 17:24:46 +0530

    model_jax_port.py: Added dimension annotations

--- modeling_MLPMixer/model_jax_port.py
+++ modeling_MLPMixer/model_jax_port.py
@@ -9,10 +9,10 @@ class MLPBlock(nn.Module):
         self.gelu = nn.GELU()
         self.dense_2 = nn.Linear(mlp_dim, input_dim)
 
-    def forward(self, x):
-        x = self.dense_1(x)
-        x = self.gelu(x)
-        x = self.dense_2(x)
+    def forward(self, x): # x ~ [:, :, input_dim]
+        x = self.dense_1(x) # x ~ [:, :, mlp_dim]
+        x = self.gelu(x) # x ~ [:, :, mlp_dim]
+        x = self.dense_2(x) # x ~ [:, :, input_dim]
         return x
 
 class MixerBlock(nn.Module):
@@ -22,13 +22,13 @@ class MixerBlock(nn.Module):
         self.token_mixing = MLPBlock(config.patch_size ** 2, config.tokens_mlp_dim)
         self.channel_mixing = MLPBlock(config.hidden_dim, config.channels_mlp_dim)
 
-    def forward(self, x):
-        y = self.layer_norm(x)
-        y = y.transpose(1, 2)
-        y = self.token_mixing(y).transpose(1, 2)
-        x = x + y
-        y = self.layer_norm(x)
-        x = x + self.channel_mixing(y)
+    def forward(self, x): # x ~ [batch_size, height * width // patch_size ^ 2, hidden_dim]
+        y = self.layer_norm(x) # y ~ [batch_size, heigth * width // patch_size ^ 2, hidden_dim]
+        y = y.transpose(1, 2) # y ~ [batch_size, hidden_dim, heigth * width // patch_size ^ 2] 
+        y = self.token_mixing(y).transpose(1, 2) # y ~ [batch_size, height * width // patch_size ^ 2, hidden_dim]
+        x = x + y # x ~ [batch_size, height * width // patch_size ^ 2, hidden_dim]
+        y = self.layer_norm(x) # y ~ [batch_size, height * width // patch_size ^ 2, hidden_dim]
+        x = x + self.channel_mixing(y) # x ~ [batch_size, height * width // patch_size ^ 2, hidden_dim]
         return x
 
 class MLPMixer(nn.Module):
@@ -45,12 +45,12 @@ class MLPMixer(nn.Module):
         self.head = nn.Linear(config.hidden_dim, config.num_classes)
 
     def forward(self, x, labels=None): # x ~ [batch_size, num_channels, height, width]
-        x = self.conv(x).flatten(2).transpose(1, 2)
+        x = self.conv(x).flatten(2).transpose(1, 2) # x ~ [batch_size, height * width // patch_size ^ 2, hidden_dim]
         for layer in self.layers:
-            x = layer(x)
-        x = self.layer_norm(x)
-        x = x.mean(dim=1)
-        logits = self.head(x)
+            x = layer(x) # x ~ [batch_size, height * width // patch_size ^ 2, hidden_dim]
+        x = self.layer_norm(x) # x ~ [batch_size, height * width // patch_size ^ 2, hidden_dim]
+        x = x.mean(dim=1) # x ~ [batch_size, hidden_dim]
+        logits = self.head(x) # logits ~ [batch_size, num_classes]
         loss = None
         if labels is not None:
             loss_fct = nn.CrossEntropyLoss()
