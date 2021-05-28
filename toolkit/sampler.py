195b60c2d71aad2cefd3896076ec3266915b94a5
Vishal Burman <vishal.a.burman23@gmail.com>
2021-05-28 12:40:56 +0530

    sampler.py: Added [SortishSampler] iter function and __len__

--- toolkit/sampler.py
+++ toolkit/sampler.py
@@ -1,11 +1,11 @@
 from torch.utils.data import Sampler
 
 class SortishSampler(Sampler):
-    def __init__(self, data, batch_size, shuffle=False):
-        pass
+    def __init__(self, data, batch_size, shuffle=True):
+        self.data, self.batch_size, self.shuffle = data, batch_size, shuffle
 
     def __len__(self):
-        pass
+        return len(self.data)
 
     def __iter__(self):
-        pass
+        return iter(sortish_sampler_indices(self.data, self.bs, shuffle=self.shuffle))
