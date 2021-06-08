fa00e365f924cef7a2d8a33024d0d045a1b2dc9d
Vishal Burman <vishal.a.burman23@gmail.com>
2021-06-08 22:07:31 +0530

    model.py: Added [Discriminator] structure

--- research/modeling_LinearGAN/model.py
+++ research/modeling_LinearGAN/model.py
@@ -8,3 +8,11 @@ class Generator(nn.Module):
 
     def forward(self):
         pass
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
