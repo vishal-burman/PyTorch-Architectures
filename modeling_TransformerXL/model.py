831c2832dea8224e8752e4136802995578db7cdd
Vishal Burman <vishal.a.burman23@gmail.com>
2021-06-08 21:34:23 +0530

    model.py: Added [TransformerXLModel] structure

---
+++ modeling_TransformerXL/model.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class TransformerXLModel(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
