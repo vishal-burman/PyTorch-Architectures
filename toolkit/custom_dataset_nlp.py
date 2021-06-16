99712b6fa58146b13a53e33556026cd747c31cde
Vishal Burman <vishal.a.burman23@gmail.com>
2021-06-16 17:06:51 +0530

    custom_dataset_nlp.py: Added DatasetPermutationLanguageModeling

--- toolkit/custom_dataset_nlp.py
+++ toolkit/custom_dataset_nlp.py
@@ -128,7 +128,7 @@ class DatasetMaskedLanguageModeling(Dataset):
         return input_ids, labels
 
 class DatasetPermutationLanguageModeling(Dataset):
-    def __init__(self, tokenizer, input_texts=None, max_input_length=16, train=True, plm=1/6, hf=True):
+    def __init__(self, tokenizer, input_texts=None, max_input_length=16, train=True, plm=1/6, max_span_length=5, hf=True):
         self.tokenizer = tokenizer
         self.plm_probability = plm
         if max_input_length % 2 != 0:
@@ -138,6 +138,8 @@ class DatasetPermutationLanguageModeling(Dataset):
             self.sents = input_texts
         else:
             self.sents = get_language_modeling_dataset(train, hf)
+        self.plm = plm
+        self.max_span_length = max_span_length
 
     def __len__(self):
         return len(self.sents)
@@ -161,8 +163,51 @@ class DatasetPermutationLanguageModeling(Dataset):
         return tokens
 
     def mask_tokens_plm(self, tokens):
-        raise NotImplementedError
+        """
+        Algorithm:
+        1.
+        2. 
+        """
 
+        if self.tokenizer.mask_token is None:
+            raise ValueError('mask token is necessary for Permutation Language Modeling')
+        input_ids = tokens['input_ids']
+        labels = input_ids.clone()
+
+        masked_indices = torch.full(labels.shape, 0, dtype=torch.bool)
+        target_mapping = torch.zeros((labels.size(0), labels.size(1), labels.size(1)), dtype=torch.float32)
+
+        for i in range(labels.size(0)):
+            cur_len = 0
+            max_len = labels.size(1)
+
+            while cur_len < max_len:
+                span_length = torch.randint(1, self.max_span_length + 1, (1,)).item()
+                context_length = int(span_length / self.plm_probability)
+                start_index = cur_len + torch.randint(context_length - span_length + 1, (1,)).item()
+                masked_indices[i, start_index : start_index + span_length] = 1
+                cur_len += context_length
+            target_mapping[i] = torch.eye(labels.size(1))
+        
+        special_tokens_mask = torch.tensor([self.tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()], dtype=torch.bool)
+        masked_indices.masked_fill_(special_tokens_mask, value=0.0)
+        if self.tokenizer._pad_token is not None:
+            padding_mask = labels.eq(self.tokenizer.pad_token_id)
+            masked_indices.masked_fill_(padding_mask, value=0.0)
+
+        non_func_mask = ~(padding_mask | special_tokens_mask)
+        input_ids[masked_indices] = self.tokenizer.mask_token_id
+        labels[~masked_indices] = -100 # We only compute loss on masked tokens
+        
+        perm_mask = torch.zeros((labels.size(0), labels.size(1), labels.size(1)), dtype=torch.float32)
+        for i in range(labels.size(0)):
+            perm_index = torch.arange(labels.size(1))
+            perm_index = perm_index.reshape((-1, labels.size(1) // 2)).transpose(0, 1)
+            perm_index = perm_index[torch.randperm(labels.size(1) // 2)]
+            perm_index = torch.flatten(perm_index.transpose(0, 1))
+            perm_index.masked_fill_(~masked_indices[i] & non_func_mask[i], -1)
+            perm_mask[i] = (perm_index.reshape((labels.size(1), 1)) <= perm_index.reshape((1, labels.size(1)))) & masked_indices[i]
+        return input_ids.long(), perm_mask, target_mapping, labels.long()
 
 class DataLoaderTextClassification:
     def __init__(self, tokenizer, max_input_length=16, train=True, split=None):
@@ -188,7 +233,7 @@ class DataLoaderCausalLanguageModeling:
 
 class DataLoaderMaskedLanguageModeling:
     def __init__(self, tokenizer, input_texts=None, max_input_length=16, train=True, mlm=0.15, hf=True):
-        self.dataset = DatasetLanguageModeling(
+        self.dataset = DatasetMaskedLanguageModeling(
                 tokenizer=tokenizer,
                 input_texts=input_texts, 
                 max_input_length=max_input_length, 
