0f9344d26fb52f7886122c63b6fa927b4d25da12
Vishal Burman <vishal.a.burman23@gmail.com>
2021-06-20 23:31:33 +0530

    model.py: Added DWConvBlock structure and nn.Sequential stages

--- modeling_MobileNetV1/model.py
+++ modeling_MobileNetV1/model.py
@@ -43,6 +43,14 @@ class ConvBlock(nn.Module):
             x = self.activ(x)
         return x
 
+class DWConvBlock(nn.Module):
+    def __init__(self,):
+        super().__init__()
+        pass
+
+    def forward(self,):
+        pass
+
 class MobileNetV1(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -56,6 +64,18 @@ class MobileNetV1(nn.Module):
             out_channels=init_block_channels,
             stride=2,
             ))
+        in_channels = init_block_channels
+        for i, channels_per_stage in enumerate(config.channels[1:]):
+            stage = nn.Sequential()
+            for j, out_channels in enumerate(channels_per_stage):
+                stride = 2 if (j == 0) and ((i != 0) or config.first_stage_stride) else 1
+                stage.add_module("unit{}".format(j + 1), DWConvBlock(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    stride=stride,
+                    dw_use_bn=config.dw_use_bn,
+                    dw_activation=config.dw_activation,
+                    ))
 
     def forward(self,):
         pass
