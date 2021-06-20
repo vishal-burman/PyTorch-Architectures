655e57cd5d7969ae24d261c6583684b9ae66be37
Vishal Burman <vishal.a.burman23@gmail.com>
2021-06-20 10:09:53 +0530

    model.py: Added structure [MobileNetV1]

---
+++ modeling_MobileNetV1/model.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class MobileNetV1(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        pass
+
+    def forward(self,):
+        pass
