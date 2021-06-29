d021692c8567cdf2dfdfa7d6fab05360b24d3cc2
Vishal Burman <vishal.a.burman23@gmail.com>
2021-06-29 22:37:20 +0530

    model.py: Added DWSConv3x3Block

--- modeling_MobileNetV2/model.py
+++ modeling_MobileNetV2/model.py
@@ -9,6 +9,14 @@ class Conv3x3Block(nn.Module):
     def forward(self,):
         pass
 
+class DWSConv3x3Block(nn.Module):
+    def __init__(self,):
+        super().__init__()
+        pass
+
+    def forward(self,):
+        pass
+
 class LinearBottleneck(nn.Module):
     def __init__(self,):
         super().__init__()
