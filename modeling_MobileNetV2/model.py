210469e3026ba01706e3e22820c567cd7f3c5bc7
Vishal Burman <vishal.a.burman23@gmail.com>
2021-06-29 22:32:07 +0530

    model.py: Added MobileNetV2 structure

--- modeling_MobileNetV2/model.py
+++ modeling_MobileNetV2/model.py
@@ -8,3 +8,11 @@ class LinearBottleneck(nn.Module):
 
     def forward(self,):
         pass
+
+class MobileNetV2(nn.Module):
+    def __init__(self,):
+        super().__init__()
+        pass
+
+    def forward(self,):
+        pass
