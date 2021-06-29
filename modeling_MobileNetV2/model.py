d0a29192e64d438fa4743bd9d61b4de0d79074cf
Vishal Burman <vishal.a.burman23@gmail.com>
2021-06-29 22:50:08 +0530

    model.py: pep8 modify

--- modeling_MobileNetV2/model.py
+++ modeling_MobileNetV2/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class Conv3x3Block(nn.Module):
     def __init__(
             self,
@@ -12,25 +13,26 @@ class Conv3x3Block(nn.Module):
             padding=1,
             bias=False,
             bn_eps=1e-5,
-            ):
+    ):
         super().__init__()
         self.conv = nn.Conv2d(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                kernel_size=kernel_size,
-                stride=stride,
-                padding=padding,
-                groups=groups,
-                bias=bias,
-                )
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            groups=groups,
+            bias=bias,
+        )
         self.bn = nn.BatchNorm2d(
-                num_features=out_channels,
-                eps=bn_eps,
-                )
+            num_features=out_channels,
+            eps=bn_eps,
+        )
         self.activ = nn.ReLU()
 
     def forward(self,):
         pass
+
 
 class DWSConv3x3Block(nn.Module):
     def __init__(self,):
@@ -40,6 +42,7 @@ class DWSConv3x3Block(nn.Module):
     def forward(self,):
         pass
 
+
 class LinearBottleneck(nn.Module):
     def __init__(self,):
         super().__init__()
@@ -47,6 +50,7 @@ class LinearBottleneck(nn.Module):
 
     def forward(self,):
         pass
+
 
 class MobileNetV2(nn.Module):
     def __init__(self,):
