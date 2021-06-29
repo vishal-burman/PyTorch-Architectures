68040b07ac322e259aea9992386b7f3276cb186e
Vishal Burman <vishal.a.burman23@gmail.com>
2021-06-29 22:48:59 +0530

    model.py: Added Conv3x3Block initializations

--- modeling_MobileNetV2/model.py
+++ modeling_MobileNetV2/model.py
@@ -2,9 +2,32 @@ import torch
 import torch.nn as nn
 
 class Conv3x3Block(nn.Module):
-    def __init__(self,):
+    def __init__(
+            self,
+            in_channels,
+            out_channels,
+            kernel_size=3,
+            stride=1,
+            groups=1,
+            padding=1,
+            bias=False,
+            bn_eps=1e-5,
+            ):
         super().__init__()
-        pass
+        self.conv = nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                groups=groups,
+                bias=bias,
+                )
+        self.bn = nn.BatchNorm2d(
+                num_features=out_channels,
+                eps=bn_eps,
+                )
+        self.activ = nn.ReLU()
 
     def forward(self,):
         pass
