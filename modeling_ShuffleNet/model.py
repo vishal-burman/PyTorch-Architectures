bc28870fb3879d91ff94a923b84c8f1e41397ae8
Vishal Burman <vishal.a.burman23@gmail.com>
2021-07-07 22:38:01 +0530

    model.py: Added Conv1x1Block structure

--- modeling_ShuffleNet/model.py
+++ modeling_ShuffleNet/model.py
@@ -1,6 +1,14 @@
 import torch
 import torch.nn as nn
 
+class Conv1x1Block(nn.Module):
+    def __init__(self,):
+        super().__init__()
+        pass
+
+    def forward(self,):
+        pass
+
 class ShuffleNet(nn.Module):
     def __init__(self,):
         super().__init__()
