b5fb78444cd0c85d01e11aad73ff5c20ea22d7ef
Vishal Burman <vishal.a.burman23@gmail.com>
2021-07-07 22:45:24 +0530

    model.py: Added DWSConv3x3Block structure

--- modeling_ShuffleNet/model.py
+++ modeling_ShuffleNet/model.py
@@ -17,6 +17,14 @@ class Conv1x1Block(nn.Module):
     def forward(self,):
         pass
 
+class DWSConv3x3Block(nn.Module):
+    def __init__(self,):
+        super().__init__()
+        pass
+
+    def forward(self,):
+        pass
+
 class ShuffleNet(nn.Module):
     def __init__(self,):
         super().__init__()
