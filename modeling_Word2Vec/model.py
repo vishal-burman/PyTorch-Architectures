f88d94b214e522620a29869a146ab949c90fc079
Vishal Burman <vishal.a.burman23@gmail.com>
2021-07-10 08:13:42 +0530

    undo last commit

--- modeling_Word2Vec/model.py
+++ modeling_Word2Vec/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class Word2Vec(nn.Module):
     def __init__(self, vocab_size, emb_size):
         """
@@ -14,7 +15,7 @@ class Word2Vec(nn.Module):
         self.W = nn.Linear(vocab_size, emb_size, bias=False)
         self.WT = nn.Linear(emb_size, vocab_size, bias=False)
 
-    def forward(self, X): # X ~ [bs, vocab_size]
-        hidden_layer = self.W(X) # hidden_layer ~ [bs, emb_size]
-        output_layer = self.WT(hidden_layer) # output_layer ~ [bs, vocab_size]
+    def forward(self, X):  # X ~ [bs, vocab_size]
+        hidden_layer = self.W(X)  # hidden_layer ~ [bs, emb_size]
+        output_layer = self.WT(hidden_layer)  # output_layer ~ [bs, vocab_size]
         return output_layer
