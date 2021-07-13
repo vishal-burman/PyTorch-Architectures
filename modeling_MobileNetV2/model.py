5a59c5ef1e0d589a40cfd9ba9bad407a5e13dc63
Vishal Burman <vishal.a.burman23@gmail.com>
2021-07-13 15:06:13 +0530

    model.py: Added 1x1 convolution block

--- modeling_MobileNetV2/model.py
+++ modeling_MobileNetV2/model.py
@@ -20,3 +20,31 @@ def conv1x1(
         groups=groups,
         bias=bias,
     )
+
+
+def conv1x1_block(
+    in_channels,
+    out_channels,
+    stride=1,
+    padding=0,
+    groups=1,
+    bias=False,
+    use_bn=True,
+    bn_eps=1e-5,
+    activation=(lambda: nn.ReLU(inplace=True)),
+):
+    """
+    1x1 version of standard convolution block
+    """
+    return ConvBlock(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=1,
+        stride=stride,
+        padding=padding,
+        groups=groups,
+        bias=bias,
+        use_bn=use_bn,
+        bn_eps=bn_eps,
+        activation=activation,
+    )
