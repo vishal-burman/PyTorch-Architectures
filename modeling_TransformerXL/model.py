9202140bb67182c74566a7e590151452b6d99854
Vishal Burman <vishal.a.burman23@gmail.com>
2021-08-08 18:30:27 +0530

    model.py: Added div_val=1 embed layer

--- modeling_TransformerXL/model.py
+++ modeling_TransformerXL/model.py
@@ -5,14 +5,39 @@ import torch.nn as nn
 class AdaptiveEmbedding(nn.Module):
     def __init__(
         self,
+        n_token,
+        d_embed,
+        d_proj,
+        cutoffs,
+        div_val,
+        sample_softmax=False,
     ):
         super().__init__()
-        pass
+        self.n_token = n_token
+        self.d_embed = d_embed
+        self.cutoffs = cutoffs + [n_token]
+        self.div_val = div_val
+        self.d_proj = d_proj
+        self.emb_scale = d_proj ** -0.5
+        self.cutoff_ends = [0] + self.cutoffs
+        self.emb_layers = nn.ModuleList()
+        self.emb_projs = nn.ParameterList()
+        if div_val == 1:
+            self.emb_layers.append(
+                nn.Embedding(n_token, d_embed, sparse=sample_softmax > 0)
+            )
+            if d_proj != d_embed:
+                self.emb_projs.append(nn.Parameter(torch.FloatTensor(d_proj, d_embed)))
 
     def forward(
         self,
+        inp,
     ):
-        pass
+        if self.div_val == 1:
+            embed = self.emb_layers[0](inp)
+            if self.d_proj != self.d_embed:
+                embed = nn.functional.linear(embed, self.emb_projs[0])
+        return embed
 
 
 class TransformerXLModel(nn.Module):
