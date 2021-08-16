152303ff95f45dc1d3aeea152a31645623ec4357
Vishal Burman <vishal.a.burman23@gmail.com>
2021-08-16 23:09:28 +0530

    model.py: Added div_val=4 transformation

--- research/modeling_AdaptiveEmbedding/model.py
+++ research/modeling_AdaptiveEmbedding/model.py
@@ -5,20 +5,31 @@ import torch.nn as nn
 class AdaptiveEmbedding(nn.Module):
     def __init__(
         self,
-        div_val=1,
         vocab_size,
         d_embed,
         d_proj,
+        div_val=1,
+        cutoffs=[512, 1024, 2048],
     ):
         super().__init__()
+        self.cutoffs = cutoffs + [vocab_size]
+        self.cutoff_ends = [0] + self.cutoffs
         self.emb_layers = nn.ModuleList()
         self.proj_layers = nn.ParameterList()
         if div_val == 1:
             self.emb_layers.append(nn.Embedding(vocab_size, d_embed))
             if d_embed != d_proj:
-                self.proj_layers.append(torch.FloatTensor(d_proj, d_embed))
+                self.proj_layers.append(
+                    nn.Parameter(torch.FloatTensor(d_proj, d_embed))
+                )
         else:
-            pass
+            for i in range(len(self.cutoffs)):
+                l_idx, r_idx = self.cutoff_ends[i], self.cutoff_ends[i + 1]
+                d_embed_i = d_embed // (div_val ** i)
+                self.emb_layers.append(nn.Embedding(r_idx - l_idx, d_embed_i))
+                self.proj_layers.append(
+                    nn.Parameter(torch.FloatTensor(d_proj, d_embed_i))
+                )
 
     def forward(
         self,
