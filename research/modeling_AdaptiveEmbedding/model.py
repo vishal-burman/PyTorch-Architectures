bcb38b70bf32524f814fadabfd795373229c6e0c
Vishal Burman <vishal.a.burman23@gmail.com>
2021-08-16 23:25:27 +0530

    model.py: Removed proj_layers

--- research/modeling_AdaptiveEmbedding/model.py
+++ research/modeling_AdaptiveEmbedding/model.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class AdaptiveEmbedding(nn.Module):
@@ -8,32 +7,22 @@ class AdaptiveEmbedding(nn.Module):
         self,
         vocab_size,
         d_embed,
-        d_proj,
         div_val=1,
         cutoffs=[512, 1024, 2048],
     ):
         super().__init__()
         self.div_val = div_val
         self.d_embed = d_embed
-        self.d_proj = d_proj
         self.cutoffs = cutoffs + [vocab_size]
         self.cutoff_ends = [0] + self.cutoffs
         self.emb_layers = nn.ModuleList()
-        self.proj_layers = nn.ParameterList()
         if div_val == 1:
             self.emb_layers.append(nn.Embedding(vocab_size, d_embed))
-            if d_embed != d_proj:
-                self.proj_layers.append(
-                    nn.Parameter(torch.FloatTensor(d_proj, d_embed))
-                )
         else:
             for i in range(len(self.cutoffs)):
                 l_idx, r_idx = self.cutoff_ends[i], self.cutoff_ends[i + 1]
                 d_embed_i = d_embed // (div_val ** i)
                 self.emb_layers.append(nn.Embedding(r_idx - l_idx, d_embed_i))
-                self.proj_layers.append(
-                    nn.Parameter(torch.FloatTensor(d_proj, d_embed_i))
-                )
 
     def forward(
         self,
@@ -41,7 +30,6 @@ class AdaptiveEmbedding(nn.Module):
     ):
         if self.div_val == 1:
             embed = self.emb_layers[0](inp)
-            if self.d_embed != self.d_proj:
-                embed = F.linear(embed, self.proj_layers[0])
         else:
             raise NotImplementedError
+        return embed
