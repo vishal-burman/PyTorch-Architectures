418943f5f3eaf05b1d0aae4c70ad6a2f68547378
Vishal Burman <vishal.a.burman23@gmail.com>
2021-08-16 22:55:40 +0530

    model.py: Added div_val=1 transformation

--- research/modeling_AdaptiveEmbedding/model.py
+++ research/modeling_AdaptiveEmbedding/model.py
@@ -5,9 +5,20 @@ import torch.nn as nn
 class AdaptiveEmbedding(nn.Module):
     def __init__(
         self,
+        div_val=1,
+        vocab_size,
+        d_embed,
+        d_proj,
     ):
         super().__init__()
-        pass
+        self.emb_layers = nn.ModuleList()
+        self.proj_layers = nn.ParameterList()
+        if div_val == 1:
+            self.emb_layers.append(nn.Embedding(vocab_size, d_embed))
+            if d_embed != d_proj:
+                self.proj_layers.append(torch.FloatTensor(d_proj, d_embed))
+        else:
+            pass
 
     def forward(
         self,
