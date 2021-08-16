76a95ccf65a6f6898d7e239b5603620a8097621c
Vishal Burman <vishal.a.burman23@gmail.com>
2021-08-16 23:38:14 +0530

    model.py: Added _test() for div_val=1

--- research/modeling_AdaptiveEmbedding/model.py
+++ research/modeling_AdaptiveEmbedding/model.py
@@ -33,3 +33,24 @@ class AdaptiveEmbedding(nn.Module):
         else:
             raise NotImplementedError
         return embed
+
+
+def _test():
+    print("Testing for div_val = 1")
+
+    d_embed = 512
+    vocab_size = 128
+    model = AdaptiveEmbedding(vocab_size=vocab_size, d_embed=d_embed)
+    model.eval()
+    with torch.set_grad_enabled(False):
+        sample_inp = torch.arange(16, dtype=torch.long).reshape(2, 8)
+        output = model(sample_inp)
+    assert output.dim() == 3, f"Output dim is {output.dim()} instead of 3"
+    assert (
+        output.size(2) == d_embed
+    ), f"Wrong output shape: {output.size(2)} instead of {d_embed}"
+    print("Testing for div_val = 1 done!")
+
+
+if __name__ == "__main__":
+    _test()
