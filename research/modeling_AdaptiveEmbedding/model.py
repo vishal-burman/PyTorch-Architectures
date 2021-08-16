58fcca840c6dea510b26f82aa59502146f5b3da8
Vishal Burman <vishal.a.burman23@gmail.com>
2021-08-16 21:58:18 +0530

    model.py: Added AdaptiveEmbedding module structure

---
+++ research/modeling_AdaptiveEmbedding/model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class AdaptiveEmbedding(nn.Module):
+    def __init__(
+        self,
+    ):
+        super().__init__()
+        pass
+
+    def forward(
+        self,
+    ):
+        pass
