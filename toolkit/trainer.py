72ac40dc29c4968115bca258caf3ef109f353a97
Vishal Burman <vishal.a.burman23@gmail.com>
2021-09-03 13:45:36 +0530

    trainer.py: Added init_optimizer helper function

--- toolkit/trainer.py
+++ toolkit/trainer.py
@@ -2,11 +2,22 @@ import torch
 import torch.nn as nn
 
 
+def init_optimizer(optimizer: str, **kwargs):
+    optimizer = optimizer.lower()
+    if optimizer == "adam":
+        return torch.optim.Adam(**kwargs)
+    elif optimizer == "adamw":
+        return torch.optim.AdamW(**kwargs)
+
+
 class Trainer:
     def __init__(
         self,
+        model,
+        optimizer,
     ):
-        pass
+        self.model = model
+        self.optimizer = init_optimizer(optimizer)
 
     def train(
         self,
