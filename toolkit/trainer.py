3128dd264594e243b6b37475e3605a54306d6b78
Vishal Burman <vishal.a.burman23@gmail.com>
2021-09-08 18:24:39 +0530

    trainer.py: Added vaidation loop

--- toolkit/trainer.py
+++ toolkit/trainer.py
@@ -2,7 +2,8 @@ from tqdm.auto import tqdm
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from .utils import get_linear_schedule_with_warmup
+from .utils import get_linear_schedule_with_warmup, dict_to_device
+from .metrics import cv_compute_accuracy, nlp_compute_accuracy, nlp_compute_mean_loss
 
 
 def init_optimizer(optimizer: str, model, lr):
@@ -44,6 +45,7 @@ class Trainer:
         shuffle_train=False,
         shuffe_valid=False,
         num_warmup_steps=0,
+        metric="nlp_perplexity",
     ):
         if not self.model.training:
             print("Model in eval mode...switching to train mode")
@@ -67,19 +69,30 @@ class Trainer:
         )
 
         for epoch in range(epochs):
+            loss_list = []
             if not self.model.training:
                 print("Model is in eval mode ... switching to train mode")
-                model.train()
+                self.model.train()
 
             for idx, sample in train_loader:
                 outputs = self.model(**dict_to_device(sample, device=self.device))
                 loss, _ = outputs[0]
+                loss_list.append(loss.item())
                 loss.backward()
 
                 optimizer.step()
                 scheduler.step()
                 optimizer.zero_grad()
                 progress_bar.update(1)
+
+            self.model.eval()
+            with torch.set_grad_enabled(False):
+                metric_output = self.validate(valid_loader, metric=metric)
+
+            mean_loss = torch.mean(torch.tensor(loss_list)).item()
+            print(
+                f"Epoch: {epoch + 1} || Training Loss: {mean_loss:.3f} || {metric}: {metric_output:.3f}"
+            )
 
     def validate(
         self,
@@ -91,10 +104,18 @@ class Trainer:
             self.model.eval()
 
         if metric == "nlp_accuracy":
-            pass
+            metric_output = nlp_compute_accuracy(
+                self.model, dataloader, device=self.device, fp16=False
+            )
         elif metric == "nlp_perplexity":
-            pass
+            metric_output = nlp_compute_mean_loss(
+                self.model, dataloader, device=self.device, fp16=False
+            )
         elif metric == "cv_accuracy":
-            pass
+            metric_output = cv_compute_accuracy(
+                self.model, dataloader, device=self.device, fp16=False
+            )
         else:
             raise NotImplementedError(f"{metric} is not supported")
+
+        return metric_output
