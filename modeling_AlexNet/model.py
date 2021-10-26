374d6d0e5e169e1cd8c9be6a493c91255659248e
Vishal Burman <vishal.a.burman23@gmail.com>
2021-10-26 10:46:32 +0530

    model.py: Updated model signatures to reflect Trainer

--- modeling_AlexNet/model.py
+++ modeling_AlexNet/model.py
@@ -2,11 +2,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class AlexNet(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes: int):
         super(AlexNet, self).__init__()
-        self.features=nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=11, stride=4, padding=2),
+        self.features = nn.Sequential(
+            nn.Conv2d(
+                in_channels=3, out_channels=64, kernel_size=11, stride=4, padding=2
+            ),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(in_channels=64, out_channels=192, kernel_size=5, padding=2),
@@ -15,28 +18,31 @@ class AlexNet(nn.Module):
             nn.Conv2d(in_channels=192, out_channels=384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True), 
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2)
-            )
-        
-        self.avgpool=nn.AdaptiveAvgPool2d((6, 6))
-        
-        self.classifier=nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+
+        self.classifier = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Linear(256*6*6, 4096),
+            nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(0.5),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes)
-            )
-    
-    def forward(self, x):
-        x=self.features(x)
-        x=self.avgpool(x)
-        x=x.view(x.size(0), 256*6*6)
-        logits=self.classifier(x)
-        probas=F.softmax(logits, dim=1)
-        return logits, probas
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x: torch.Tensor, labels: Optional[torch.Tensor] = None):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        logits = self.classifier(x)
+        loss = None
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits.view(logits.size(0), -1), labels.view(-1))
+        return loss, logits
