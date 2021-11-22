a3ec44bdd3c0c9ffd47ca56b576e57ae16405349
Vishal Burman <vishal.a.burman23@gmail.com>
2021-11-22 08:18:23 +0530

    clusterizer.py: Added _init_pipeline method for easy initialization

--- toolkit/clusterizer.py
+++ toolkit/clusterizer.py
@@ -1,5 +1,9 @@
+import logging
+
 import fire
 from tqdm import trange
+
+logger = logging.get_logger(__name__)
 
 
 def make_batch(corpus_sentences: List[str], batch_size: int):
@@ -14,7 +18,18 @@ def make_batch(corpus_sentences: List[str], batch_size: int):
     sentences_sorted = [corpus_sentences[idx] for idx in length_sorted_idx]
 
     for start_index in trange(0, len(corpus_sentences), batch_size, desc="Batches"):
-        pass
+        sentences_batch = corpus_sentences[start_index : start_index + batch_size]
+
+
+def _init_pipeline(model_name: str):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name)
+    model.eval()
+
+    params = sum(p.numel() for p in model.parameters())
+    logger.info(f"Total Parameters = {params}")
+
+    return tokenizer, model
 
 
 def _get_length(text: Union[str, List[str]]):
