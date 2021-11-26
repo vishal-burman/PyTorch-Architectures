774febe8beda39bade3e59393b352bff1ec66482
Vishal Burman <vishal.a.burman23@gmail.com>
2021-11-26 13:51:20 +0530

    clusterizer.py: numpy cast to float32

--- toolkit/clusterizer.py
+++ toolkit/clusterizer.py
@@ -152,7 +152,7 @@ def _community_detection(
     )  # TODO fix OOM error when large corpus
 
     if isinstance(cosine_scores, np.ndarray):
-        cosine_scores = torch.from_numpy(cosine_scores, dtype=torch.float64)
+        cosine_scores = torch.from_numpy(cosine_scores)
     top_k_values, _ = cosine_scores.topk(k=min_community_size, largest=True)
 
     extracted_communities = []
@@ -216,16 +216,25 @@ def _calculate_cs_torch(a: torch.Tensor, b: torch.Tensor):
 
 def _calculate_cs_numpy(a: np.ndarray, b: np.ndarray):
     assert a.shape == b.shape, f"Shape of a: {a.shape} and Shape of b: {b.shape}"
+    a_copy = a.astype(np.float32)
+    b_copy = b.astype(np.float32)
 
-    non_zero_vector = np.full((a.shape), 1e-12, dtype=np.float64)  # Prevent div by zero
-    a_norm = a / np.maximum(
-        np.repeat(np.linalg.norm(a, axis=1, keepdims=True), a.shape[1], axis=1),
+    non_zero_vector = np.full((a.shape), 1e-12, dtype=np.float32)  # Prevent div by zero
+    a_norm = a_copy / np.maximum(
+        np.repeat(
+            np.linalg.norm(a_copy, axis=1, keepdims=True), a_copy.shape[1], axis=1
+        ),
         non_zero_vector,
     )
-    b_norm = b / np.maximum(
-        np.repeat(np.linalg.norm(b, axis=1, keepdims=True), b.shape[1], axis=1),
+    b_norm = b_copy / np.maximum(
+        np.repeat(
+            np.linalg.norm(b_copy, axis=1, keepdims=True), b_copy.shape[1], axis=1
+        ),
         non_zero_vector,
     )
+
+    assert a_copy.dtype == a_norm.dtype
+    assert b_copy.dtype == b_norm.dtype
     return a_norm @ b_norm.T
 
 
