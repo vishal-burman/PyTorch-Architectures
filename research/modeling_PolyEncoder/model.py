9ca9934757eaea801a49d320e829150e329657db
Vishal Burman <vishal.a.burman23@gmail.com>
2021-12-27 17:05:47 +0530

    model.py: Add Encode nn Module

---
+++ research/modeling_PolyEncoder/model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class Encode(nn.Module):
+    def __init__(
+        self,
+    ):
+        super().__init__()
+        pass
+
+    def forward(
+        self,
+    ):
+        pass
