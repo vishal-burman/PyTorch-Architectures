107f3f65b876213330b8b81246e23ab479840897
Vishal Burman <vishal.a.burman23@gmail.com>
2021-12-27 17:18:00 +0530

    model.py: Add AutoModel and AutoTokenizer with max_embeddings

--- research/modeling_PolyEncoder/model.py
+++ research/modeling_PolyEncoder/model.py
@@ -1,15 +1,23 @@
 import torch
 import torch.nn as nn
+from transformers import AutoModel
 
 
 class Encode(nn.Module):
     def __init__(
         self,
+        encoder_name: str = "distilbert-base-uncased",
     ):
         super().__init__()
-        pass
+        self.tokenizer = AutoTokenizer.from_pretrained(encoder_name)
+        self.encoder = AutoModel.from_pretrained(encoder_name)
+        if "max_position_embeddings" in self.encoder.config.to_dict():
+            self.max_length = self.encoder.max_position_embeddings
+        else:
+            raise NotImplementedError
 
     def forward(
         self,
+        text: str,
     ):
         pass
