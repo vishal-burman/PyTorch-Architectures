4ec4d0b4e23d5442447aebf4a0c10a7a9f247928
Vishal Burman <vishal.a.burman23@gmail.com>
2021-12-27 18:40:38 +0530

    model.py: return first token pooled output

--- research/modeling_PolyEncoder/model.py
+++ research/modeling_PolyEncoder/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from transformers import AutoModel
+from transformers import AutoModel, AutoTokenizer
 
 
 class Encode(nn.Module):
@@ -12,7 +12,7 @@ class Encode(nn.Module):
         self.tokenizer = AutoTokenizer.from_pretrained(encoder_name)
         self.encoder = AutoModel.from_pretrained(encoder_name)
         if "max_position_embeddings" in self.encoder.config.to_dict():
-            self.max_length = self.encoder.max_position_embeddings
+            self.max_length = self.encoder.config.max_position_embeddings
         else:
             raise NotImplementedError
 
@@ -20,4 +20,14 @@ class Encode(nn.Module):
         self,
         text: str,
     ):
-        pass
+        tokens = self.tokenizer(
+            text,
+            max_length=self.max_length,
+            padding=True,
+            truncation=True,
+            return_tensors="pt",
+        )
+        outputs = self.encoder(**tokens)
+        logits = outputs.last_hidden_state
+        pooled_output = logits[:, 0]
+        return pooled_output
