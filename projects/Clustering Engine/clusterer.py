f97dab894e10a4ba39de53449c85db1d598ce8c1
Vishal Burman <vishal.a.burman23@gmail.com>
2022-11-05 19:41:59 +0530

    clusterer.py: Add encode_sentences for batch-encode of chunks

--- projects/Clustering Engine/clusterer.py
+++ projects/Clustering Engine/clusterer.py
@@ -7,15 +7,19 @@ class Clusterer:
     def __init__(self, sentence_encoder: str = "all-MiniLM-L12-v2"):
         self.model = SentenceTransformer(sentence_encoder)
 
-    def create_chunks(self, sentences: List[str], chunk_size: int = 10000, verbose: bool = False):
+    def create_chunks(self, sentences: List[str], chunk_size: int = 10000, verbose: bool = False) -> List[List[str]]:
         chunks = [sentences[i: i + chunk_size]
-                   for i in range(0, len(sentences), chunk_size)]
+                  for i in range(0, len(sentences), chunk_size)]
 
         if verbose:
             print(
                 f"No of windows created from {len(sentences)}: {len(chunks)}")
 
         return chunks
+
+    def encode_sentences(self, sentences: List[str], verbose: bool = False):
+        sentences_embeds = self.model.encode(sentences, batch_size=128, show_progress_bar=verbose)
+        return sentences_embeds
 
     def cluster(self, sentences: List[str]):  # TODO define a return type
         chunks = self.create_chunks(sentences)
