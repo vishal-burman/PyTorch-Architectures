9cd91a3647587cbd652d12b1b2f532f38322a72f
Vishal Burman <vishal.a.burman23@gmail.com>
2022-11-27 11:03:47 +0530

    clusterer.py: Switch to black + isort formatting

--- projects/Clustering Engine/clusterer.py
+++ projects/Clustering Engine/clusterer.py
@@ -8,20 +8,24 @@ class Clusterer:
     def __init__(self, sentence_encoder: str = "all-MiniLM-L12-v2"):
         self.model = SentenceTransformer(sentence_encoder)
 
-    def create_chunks(self, sentences: List[str], chunk_size: int = 10000, verbose: bool = False) -> List[List[str]]:
-        chunks = [sentences[i: i + chunk_size]
-                  for i in range(0, len(sentences), chunk_size)]
+    def create_chunks(
+        self, sentences: List[str], chunk_size: int = 10000, verbose: bool = False
+    ) -> List[List[str]]:
+        chunks = [
+            sentences[i : i + chunk_size] for i in range(0, len(sentences), chunk_size)
+        ]
 
         if verbose:
-            print(
-                f"No of windows created from {len(sentences)}: {len(chunks)}")
+            print(f"No of windows created from {len(sentences)}: {len(chunks)}")
 
         return chunks
 
     def encode_sentences(self, sentences: List[str], verbose: bool = False) -> np.array:
-        sentences_embeds = self.model.encode(sentences, batch_size=128, show_progress_bar=verbose)
+        sentences_embeds = self.model.encode(
+            sentences, batch_size=128, show_progress_bar=verbose
+        )
         return sentences_embeds
-    
+
     def create_signature(self, sentences_embeds: np.array):
         if sentences_embeds.ndim == 1:
             sentences_embeds = sentences_embeds.reshape(1, -1)
