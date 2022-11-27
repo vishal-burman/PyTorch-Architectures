975a65acfd914e5308e22e89ff3f451e6913416f
Vishal Burman <vishal.a.burman23@gmail.com>
2022-11-27 20:52:19 +0530

    clusterer.py: Remove cluster_model in __init__ function

--- projects/Clustering Engine/clusterer.py
+++ projects/Clustering Engine/clusterer.py
@@ -15,12 +15,6 @@ class ChunkRecord:
 class Clusterer:
     def __init__(self, sentence_encoder: str = "all-MiniLM-L12-v2"):
         self.model = SentenceTransformer(sentence_encoder)
-        self.cluster_model = AgglomerativeClustering(
-            n_clusters=None,
-            affinity="cosine",
-            linkage="average",
-            distance_threshold=0.3,
-        )
 
     def create_chunks(
         self, sentences: List[str], chunk_size: int, verbose: bool = False
