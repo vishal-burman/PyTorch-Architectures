cec8c497464609dffc50be58733593ab741c7277
Vishal Burman <vishal.a.burman23@gmail.com>
2022-11-27 19:59:56 +0530

    clusterer.py: Add normalize_embeddings as a user parameter

--- projects/Clustering Engine/clusterer.py
+++ projects/Clustering Engine/clusterer.py
@@ -19,18 +19,30 @@ class Clusterer:
         ]
 
         if verbose:
-            print(f"No of windows created from {len(sentences)} sentences: {len(chunks)}")
+            print(
+                f"No of windows created from {len(sentences)} sentences: {len(chunks)}"
+            )
 
         return chunks
-    
-    def normalize_embeddings(self, sentences_embeds: np.array) -> np.array:
-        sentences_embeds = sentences_embeds / np.linalg.norm(sentences_embeds, axis=1, keepdims=True)
-        pass
 
-    def encode_sentences(self, sentences: List[str], verbose: bool = False) -> np.array:
+    def normalize_embeddings(self, sentences_embeds: np.array) -> np.array:
+        sentences_embeds = sentences_embeds / np.linalg.norm(
+            sentences_embeds, axis=1, keepdims=True
+        )
+        return sentences_embeds
+
+    def encode_sentences(
+        self,
+        sentences: List[str],
+        normalize_embeddings: bool,
+        verbose: bool = False,
+    ) -> np.array:
         sentences_embeds = self.model.encode(
             sentences, batch_size=128, show_progress_bar=verbose
         )
+        if normalize_embeddings:
+            sentences_embeds = self.normalize_embeddings(sentences_embeds)
+
         return sentences_embeds
 
     def create_signature(self, sentences_embeds: np.array, verbose: bool = False):
@@ -41,11 +53,16 @@ class Clusterer:
         return sentences_signature
 
     def cluster(
-        self, sentences: List[str], chunk_size: int = 10000, verbose: bool = False
+        self,
+        sentences: List[str],
+        chunk_size: int = 10000,
+        normalize_embeddings: bool = True,
+        verbose: bool = False,
     ):  # TODO define a return type
         chunks = self.create_chunks(sentences, chunk_size=chunk_size, verbose=verbose)
         chunks_embeds = [
-            self.encode_sentences(chunk, verbose=verbose) for chunk in chunks
+            self.encode_sentences(chunk, normalize_embeddings, verbose=verbose)
+            for chunk in chunks
         ]
         chunks_signature = [self.create_signature(ce) for ce in chunks_embeds]
         assert len(chunks_embeds) == len(chunks_signature)
